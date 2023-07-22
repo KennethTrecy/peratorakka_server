2b4ffb1014ec897b85d0582c6633eded51b60b70
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-22 15:39:50 +0800

    test(feature): add sample test

---
+++ t/Feature/Authentication/RegisterTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Authentication;
+
+use Tests\Feature\Helper\HTTPTestCase;
+
+/**
+ * @internal
+ */
+final class RegisterTest extends HTTPTestCase
+{
+    public function testValidDetails()
+    {
+        $this->assertTrue(true);
+    }
+}
