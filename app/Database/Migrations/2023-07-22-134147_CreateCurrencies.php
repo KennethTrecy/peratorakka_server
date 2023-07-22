a0c01546b5f1f684ff5bd8eece7228687c4dba63
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-22 21:50:31 +0800

    internal(currency): make migration

---
+++ app/Database/Migrations/2023-07-22-134147_CreateCurrencies.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCurrencies extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            "id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+                "auto_increment" => true,
+            ],
+            "user_id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+            ],
+            "code" => [
+                "type" => "VARCHAR",
+                "constraint" => "255",
+                "unique" => true,
+            ],
+            "name" => [
+                "type" => "VARCHAR",
+                "constraint" => "255",
+                "unique" => true,
+            ]
+        ]);
+        $this->forge->addPrimaryKey("id");
+        $this->forge->addForeignKey("user_id", "users", "id", "CASCADE", "CASCADE");
+        $this->forge->createTable("currencies");
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable("currencies");
+    }
+}
