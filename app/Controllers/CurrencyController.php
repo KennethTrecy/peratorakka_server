ed12c4421db4a17b721971aaad6171372cec7f4c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-23 15:27:05 +0800

    internal(currency): make mechanism to show specific resource

--- app/Controllers/CurrencyController.php
+++ app/Controllers/CurrencyController.php
@@ -13,10 +13,61 @@ class CurrencyController extends BaseController
 
         $currency_model = model(CurrencyModel::class);
 
-        $data = [
+        $response_document = [
             "currencies" => $currency_model->where("user_id", $current_user->id)->findAll()
         ];
 
-        return response()->setJSON($data);
+        return response()->setJSON($response_document);
+    }
+
+    public function show(int $id)
+    {
+        $current_user = auth()->user();
+        $validation = single_service("validation");
+        $validation->setRule("currency", "code", [
+            "required",
+            "array"
+        ]);
+        $validation->setRule("currency.code", "code", [
+            "required",
+            "alpha_numeric"
+        ]);
+        $validation->setRule("currency.name", "name", [
+            "required",
+            "alpha_numeric"
+        ]);
+
+        $request_document = $request->getJson(true);
+        $is_success = $validation->run($request->getJson(true));
+
+        if ($is_success) {
+            $currency_model = model(CurrencyModel::class);
+
+            $response_document = [
+                "currencies" => $currency_model->insert(
+                    array_merge(
+                        [
+                            "user_id" => $current_user->id
+                        ],
+                        $request_document
+                    )
+                )->findAll()
+            ];
+
+            return response()->setJSON($response_document);
+        } else {
+            $raw_errors = $validation->getErrors();
+            $formalized_errors = [];
+            foreach ($raw_errors as $field => $message) {
+                array_push($formalized_errors, [
+                    "field" => $field,
+                    "message" => $message
+                ]);
+            }
+
+            return response()->failValidationError()->setJSON([
+                "errors" => $formalized_errors
+            ]);
+        }
     }
 }
