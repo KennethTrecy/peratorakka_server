cbc94e787b945479a7158555ee60d0914d3720af
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-23 15:54:16 +0800

    test(feature): ensure route to show currency works

--- t/Feature/Resource/CurrencyTest.php
+++ t/Feature/Resource/CurrencyTest.php
@@ -12,10 +12,6 @@ class CurrencyTest extends AuthenticatedHTTPTestCase
     public function testDefaultIndex()
     {
         $authenticated_info = $this->makeAuthenticatedInfo();
-        $provider = model(setting("Auth.userProvider"));
-        $this->seeInDatabase("users", [
-            "id" => $authenticated_info->getUser()->id
-        ]);
 
         $currency_fabricator = new Fabricator(CurrencyModel::class);
         $currency_fabricator->setOverrides([
@@ -23,13 +19,30 @@ class CurrencyTest extends AuthenticatedHTTPTestCase
         ]);
         $currencies = $currency_fabricator->create(10);
 
-        $result = $authenticated_info->getRequest()->withHeaders([
-            "Accept" => "application/json"
-        ])->get("/api/v1/currencies");
+        $result = $authenticated_info->getRequest()->get("/api/v1/currencies");
 
         $result->assertOk();
         $result->assertJSONExact([
             "currencies" => json_decode(json_encode($currencies))
+        ]);
+    }
+
+    public function testDefaultShow()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $currency_id = $currency["id"];
+
+        $result = $authenticated_info->getRequest()->get("/api/v1/currencies/$currency_id");
+
+        $result->assertOk();
+        $result->assertJSONExact([
+            "currency" => json_decode(json_encode($currency))
         ]);
     }
 }
