f180133dd4847cd4bb4e46a0dbf09ec2f16d727e
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-23 22:42:38 +0800

    refactor(model): separate resource model

---
+++ app/Models/BaseResourceModel.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+use CodeIgniter\Shield\Entities\User;
+use CodeIgniter\Test\Interfaces\FabricatorModel;
+use Faker\Generator;
+
+use App\Contracts\OwnedResource;
+
+abstract class BaseResourceModel extends Model implements FabricatorModel, OwnedResource
+{
+    protected $primaryKey = "id";
+    protected $useAutoIncrement = true;
+    protected $useSoftDeletes = true;
+    protected $protectFields = true;
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat = "datetime";
+    protected $createdField = "created_at";
+    protected $updatedField = "updated_at";
+    protected $deletedField = "deleted_at";
+
+    // Validation
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+
+    abstract protected function limitSearchToUser($builder, User $user);
+
+    public function isOwnedBy(User $user, string $search_mode, int $resource_id): bool {
+        $match = $this
+            ->limitSearchToUser($this->getSearchQuery($search_mode), $user)
+            ->find($resource_id);
+        return !is_null($match);
+    }
+
+    private function getSearchQuery(string $search_mode) {
+        if ($search_mode === SEARCH_WITH_DELETED) return $this->withDeleted();
+        else if ($search_mode === SEARCH_ONLY_DELETED) return $this->onlyDeleted();
+
+        return $this;
+    }
+}
--- app/Models/CurrencyModel.php
+++ app/Models/CurrencyModel.php
@@ -2,51 +2,21 @@
 
 namespace App\Models;
 
-use CodeIgniter\Model;
 use CodeIgniter\Shield\Entities\User;
-use CodeIgniter\Test\Interfaces\FabricatorModel;
 use Faker\Generator;
 
 use App\Contracts\OwnedResource;
 
-class CurrencyModel extends Model implements FabricatorModel, OwnedResource
+class CurrencyModel extends BaseResourceModel
 {
     protected $table            = "currencies";
-    protected $primaryKey       = "id";
-    protected $useAutoIncrement = true;
     protected $returnType       = "array";
-    protected $useSoftDeletes   = true;
-    protected $protectFields    = true;
     protected $allowedFields    = [
         "user_id",
         "code",
         "name",
         "deleted_at"
     ];
-
-    // Dates
-    protected $useTimestamps = true;
-    protected $dateFormat    = "datetime";
-    protected $createdField  = "created_at";
-    protected $updatedField  = "updated_at";
-    protected $deletedField  = "deleted_at";
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 
     public function fake(Generator &$faker)
     {
@@ -56,18 +26,7 @@ class CurrencyModel extends Model implements FabricatorModel, OwnedResource
         ];
     }
 
-    public function isOwnedBy(User $user, string $search_mode, int $resource_id): bool {
-        $match = $this
-            ->getSearchQuery($search_mode)
-            ->where("user_id", $user->id)
-            ->find($resource_id);
-        return !is_null($match);
-    }
-
-    private function getSearchQuery(string $search_mode) {
-        if ($search_mode === SEARCH_WITH_DELETED) return $this->withDeleted();
-        else if ($search_mode === SEARCH_ONLY_DELETED) return $this->onlyDeleted();
-
-        return $this;
+    protected function limitSearchToUser($builder, User $user) {
+        return $this->where("user_id", $user->id);
     }
 }
