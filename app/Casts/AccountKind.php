52eda851d1723cf095e7cb97f6728f00c985358c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-24 13:58:56 +0800

    fix(account): use unknown account kind as fallback

--- app/Casts/AccountKind.php
+++ app/Casts/AccountKind.php
@@ -8,7 +8,9 @@ class AccountKind extends BaseCast
 {
     public static function get($value, array $params = [])
     {
-        return ACCOUNT_KINDS[$value];
+        return isset(ACCOUNT_KINDS[$value])
+            ? ACCOUNT_KINDS[$value]
+            : UNKNOWN_ACCOUNT_KIND;
     }
 
     public static function set($value, array $params = [])
