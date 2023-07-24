678469adac63e7e3302f45c7058b18d90f429c62
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-24 14:06:35 +0800

    internal(account): make model

---
+++ app/Models/AccountModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Shield\Entities\User;
+use Faker\Generator;
+
+use App\Entities\Account;
+
+class AccountModel extends BaseResourceModel
+{
+    protected $table = "currencies";
+    protected $returnType = Account::class;
+    protected $allowedFields = [
+        "currency_id",
+        "name",
+        "description",
+        "kind",
+        "deleted_at"
+    ];
+
+    public function fake(Generator &$faker)
+    {
+        return [
+            "name"  => $faker->unique()->firstName(),
+            "description"  => $faker->paragraph(),
+            "kind"  => $faker->randomElement(ACCEPTABLE_ACCOUNT_KINDS),
+        ];
+    }
+
+    public function limitSearchToUser(BaseResourceModel $query_builder, User $user) {
+        return $query_builder
+            ->whereIn(
+                "currency_id",
+                model(Currency::class)
+                    ->select("id")
+                    ->where("user_id", $user->id)
+            );
+    }
+}
