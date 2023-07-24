915e5d747bf7daba6e99affe1c20c2439aa71e3d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-24 11:01:44 +0800

    refactor(controller): make generalized owned resource controller

---
+++ app/Controllers/BaseOwnedResourceController.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\Validation\Validation;
+
+use App\Contracts\OwnedResource;
+use App\Controllers\BaseController;
+
+abstract class BaseOwnedResourceController extends BaseController
+{
+    use ResponseTrait;
+
+    abstract protected static function getIndividualName(): string;
+    abstract protected static function getCollectiveName(): string;
+    abstract protected static function getModel(): OwnedResource;
+    abstract protected static function makeValidation(): Validation;
+
+    public function index()
+    {
+        $current_user = auth()->user();
+
+        $model = static::getModel();
+
+        $response_document = [
+            static::getCollectiveName() => $model
+                ->limitSearchToUser($model, $current_user)
+                ->findAll()
+        ];
+
+        return response()->setJSON($response_document);
+    }
+
+    public function show(int $id)
+    {
+        $current_user = auth()->user();
+        $model = static::getModel();
+        $data = $model->find($id);
+
+        $is_success = !is_null($data);
+
+        if ($is_success) {
+            $response_document = [
+                static::getIndividualName() => $model->find($id)
+            ];
+
+            return response()->setJSON($response_document);
+        } else {
+            return $this->failNotFound()->setJSON([
+                "errors" => [
+                    [
+                        "message" => "The requested resource was not found."
+                    ]
+                ]
+            ]);
+        }
+    }
+
+    public function create()
+    {
+        $controller = $this;
+        return $this->processValidInputsOnly(function($request_data) use ($controller) {
+            $current_user = auth()->user();
+
+            $model = static::getModel();
+            $info = array_merge(
+                [ "user_id" => $current_user->id ],
+                $request_data
+            );
+
+            $is_success = $model->insert($info, false);
+            if ($is_success) {
+                $response_document = [
+                    static::getIndividualName() => array_merge(
+                        [ "id" =>  $model->getInsertID() ],
+                        $info
+                    )
+                ];
+
+                return $controller->respondCreated()->setJSON($response_document);
+            }
+
+            return $controller->makeServerError(
+                "There is an error on inserting to the database server."
+            );
+        });
+    }
+
+    public function update(int $id)
+    {
+        $controller = $this;
+        return $this->processValidInputsOnly(function($request_data) use ($controller, $id) {
+            $current_user = auth()->user();
+
+            $model = static::getModel();
+            $info = array_merge(
+                [ "user_id" =>  $current_user->id ],
+                $request_data
+            );
+
+            $is_success = $model->update($id, $info);
+            if ($is_success) {
+                return $controller->respondNoContent();
+            }
+
+            return $controller->makeServerError(
+                "There is an error on updating to the database server."
+            );
+        });
+    }
+
+    public function delete(int $id)
+    {
+        $model = static::getModel();
+
+        $is_success = $model->delete($id);
+        if ($is_success) {
+            return $this->respondNoContent();
+        }
+
+        return $this->makeServerError(
+            "There is an error on deleting to the database server."
+        );
+    }
+
+    public function restore(int $id)
+    {
+        $model = static::getModel();
+
+        $is_success = $model->update($id, [ "deleted_at" => null ]);
+        if ($is_success) {
+            return $this->respondNoContent();
+        }
+
+        return $this->makeServerError(
+            "There is an error on restoring to the database server."
+        );
+    }
+
+    public function forceDelete(int $id)
+    {
+        $model = static::getModel();
+
+        $is_success = $model->delete($id, true);
+        if ($is_success) {
+            return $this->respondNoContent();
+        }
+
+        return $this->makeServerError(
+            "There is an error on force deleting to the database server."
+        );
+    }
+
+    private function processValidInputsOnly(callable $operation)
+    {
+        $validation = static::makeValidation();
+
+        $request_document = $this->request->getJson(true);
+        $is_success = $validation->run($request_document);
+
+        if ($is_success) {
+            return call_user_func($operation, $request_document[static::getIndividualName()]);
+        }
+
+        $raw_errors = $validation->getErrors();
+        $formalized_errors = [];
+        foreach ($raw_errors as $field => $message) {
+            array_push($formalized_errors, [
+                "field" => $field,
+                "message" => $message
+            ]);
+        }
+
+        return $this->failValidationError()->setJSON([
+            "errors" => $formalized_errors
+        ]);
+    }
+
+    private function makeServerError(string $development_message) {
+        return $this->failServerError()->setJSON([
+            "errors" => [
+                [
+                    "message" => $this->request->getServer("CI_ENVIRONMENT") === "development"
+                        ? $development_message
+                        : "Please contact the developer because there is an error."
+                ]
+            ]
+        ]);
+    }
+}
--- app/Controllers/CurrencyController.php
+++ app/Controllers/CurrencyController.php
@@ -2,194 +2,40 @@
 
 namespace App\Controllers;
 
-use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\Validation\Validation;
 
-use App\Controllers\BaseController;
+use App\Contracts\OwnedResource;
 use App\Models\CurrencyModel;
 
-class CurrencyController extends BaseController
+class CurrencyController extends BaseOwnedResourceController
 {
-    use ResponseTrait;
-
-    public function index()
-    {
-        $current_user = auth()->user();
-
-        $currency_model = model(CurrencyModel::class);
-
-        $response_document = [
-            "currencies" => $currency_model->where("user_id", $current_user->id)->findAll()
-        ];
-
-        return response()->setJSON($response_document);
+    protected static function getIndividualName(): string {
+        return "currency";
     }
 
-    public function show(int $id)
-    {
-        $current_user = auth()->user();
-        $currency_model = model(CurrencyModel::class);
-        $currency_data = $currency_model->find($id);
-
-        $is_success = !is_null($currency_data);
-
-        if ($is_success) {
-            $response_document = [
-                "currency" => $currency_model->find($id)
-            ];
-
-            return response()->setJSON($response_document);
-        } else {
-            return $this->failNotFound()->setJSON([
-                "errors" => [
-                    [
-                        "message" => "The requested resource was not found."
-                    ]
-                ]
-            ]);
-        }
+    protected static function getCollectiveName(): string {
+        return "currencies";
     }
 
-    public function create()
-    {
-        $controller = $this;
-        return $this->processValidInputsOnly(function($request_data) use ($controller) {
-            $current_user = auth()->user();
-
-            $currency_model = model(CurrencyModel::class);
-            $currency_info = array_merge(
-                [ "user_id" => $current_user->id ],
-                $request_data
-            );
-
-            $is_success = $currency_model->insert($currency_info, false);
-            if ($is_success) {
-                $response_document = [
-                    "currency" => array_merge(
-                        [ "id" =>  $currency_model->getInsertID() ],
-                        $currency_info
-                    )
-                ];
-
-                return $controller->respondCreated()->setJSON($response_document);
-            }
-
-            return $controller->makeServerError(
-                "There is an error on inserting to the database server."
-            );
-        });
+    protected static function getModel(): OwnedResource {
+        return model(CurrencyModel::class);
     }
 
-    public function update(int $id)
-    {
-        $controller = $this;
-        return $this->processValidInputsOnly(function($request_data) use ($controller, $id) {
-            $current_user = auth()->user();
-
-            $currency_model = model(CurrencyModel::class);
-            $currency_info = array_merge(
-                [ "user_id" =>  $current_user->id ],
-                $request_data
-            );
-
-            $is_success = $currency_model->update($id, $currency_info);
-            if ($is_success) {
-                return $controller->respondNoContent();
-            }
-
-            return $controller->makeServerError(
-                "There is an error on updating to the database server."
-            );
-        });
-    }
-
-    public function delete(int $id)
-    {
-        $currency_model = model(CurrencyModel::class);
-
-        $is_success = $currency_model->delete($id);
-        var_dump("normal del", json_encode(compact("id")));
-        if ($is_success) {
-            return $this->respondNoContent();
-        }
-
-        return $this->makeServerError(
-            "There is an error on deleting to the database server."
-        );
-    }
-
-    public function restore(int $id)
-    {
-        $currency_model = model(CurrencyModel::class);
-
-        $is_success = $currency_model->update($id, [ "deleted_at" => null ]);
-        if ($is_success) {
-            return $this->respondNoContent();
-        }
-
-        return $this->makeServerError(
-            "There is an error on restoring to the database server."
-        );
-    }
-
-    public function forceDelete(int $id)
-    {
-        $currency_model = model(CurrencyModel::class);
-
-        $is_success = $currency_model->delete($id, true);
-        if ($is_success) {
-            return $this->respondNoContent();
-        }
-
-        return $this->makeServerError(
-            "There is an error on force deleting to the database server."
-        );
-    }
-
-    private function processValidInputsOnly(callable $operation)
-    {
+    protected static function makeValidation(): Validation {
         $validation = single_service("validation");
-        $validation->setRule("currency", "currency info", [
+        $individual_name = static::getIndividualName();
+        $validation->setRule($individual_name, "currency info", [
             "required"
         ]);
-        $validation->setRule("currency.code", "code", [
+        $validation->setRule("$individual_name.code", "code", [
             "required",
             "alpha_numeric"
         ]);
-        $validation->setRule("currency.name", "name", [
+        $validation->setRule("$individual_name.name", "name", [
             "required",
             "alpha_numeric"
         ]);
 
-        $request_document = $this->request->getJson(true);
-        $is_success = $validation->run($request_document);
-
-        if ($is_success) {
-            return call_user_func($operation, $request_document["currency"]);
-        }
-
-        $raw_errors = $validation->getErrors();
-        $formalized_errors = [];
-        foreach ($raw_errors as $field => $message) {
-            array_push($formalized_errors, [
-                "field" => $field,
-                "message" => $message
-            ]);
-        }
-
-        return $this->failValidationError()->setJSON([
-            "errors" => $formalized_errors
-        ]);
-    }
-
-    private function makeServerError(string $development_message) {
-        return $this->failServerError()->setJSON([
-            "errors" => [
-                [
-                    "message" => $this->request->getServer("CI_ENVIRONMENT") === "development"
-                        ? $development_message
-                        : "Please contact the developer because there is an error."
-                ]
-            ]
-        ]);
+        return $validation;
     }
 }
