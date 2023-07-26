8e63b65cdd10c2d9ef8a0ea532362a001de893b1
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-26 15:38:24 +0800

    fix(test): look on the number of records first

--- t/Feature/Authentication/RegisterTest.php
+++ t/Feature/Authentication/RegisterTest.php
@@ -24,15 +24,14 @@ class RegisterTest extends HTTPTestCase
         ])->post("register", $user_data);
 
         $result->assertRedirect();
-        sleep(2);
+        $this->seeNumRecords(1, "users", []);
+        $this->seeNumRecords(1, "auth_identities", []);
+        $this->seeNumRecords(0, "auth_logins", []);
         $this->seeInDatabase("users", [
             "username" => $user_data["username"]
         ]);
         $this->seeInDatabase("auth_identities", [
             "secret" => $user_data["email"]
         ]);
-        $this->seeNumRecords(1, "users", []);
-        $this->seeNumRecords(1, "auth_identities", []);
-        $this->seeNumRecords(0, "auth_logins", []);
     }
 }
