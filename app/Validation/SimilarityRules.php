009eaca042f93bcfb9d1fe34b0d0e309e34edf3b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-26 16:18:32 +0800

    internal(validation): make checker by using financial entry

--- app/Validation/SimilarityRules.php
+++ app/Validation/SimilarityRules.php
@@ -3,11 +3,11 @@
 namespace App\Validation;
 
 use App\Models\AccountModel;
-use App\Models\CurrencyModel;
+use App\Models\FinancialEntryModel;
 use App\Models\ModifierModel;
 
 class SimilarityRules {
-    public function must_be_same_amount_if_opposite_account_is_on_same_currency(
+    public function must_be_same_for_modifier(
         $debit_value,
         string $parameters,
         array $data,
@@ -31,6 +31,41 @@ class SimilarityRules {
         if ($credit_value === $debit_value) return true;
 
         $modifier_id = dot_array_search($parameters[0], $data);
+
+        return $this->mayAllowForDualCurrency($modifier_id);
+    }
+
+    public function must_be_same_for_financial_entry(
+        $debit_value,
+        string $parameters,
+        array $data,
+        ?string &$error = null
+    ): bool {
+        helper("array");
+
+        $parameters = explode(",", $parameters);
+
+        if (
+            count($parameters) < 2
+            || is_null(dot_array_search($parameters[0], $data))
+            || is_null(dot_array_search($parameters[1], $data))
+        ) {
+            $error = '"{0}" needs a key to financial entry ID and key to credit value'
+                .'to check the required similarity for {field}.';
+            return false;
+        }
+
+        $credit_value = dot_array_search($parameters[1], $data);
+        if ($credit_value === $debit_value) return true;
+
+        $financial_entry_id = dot_array_search($parameters[0], $data);
+        $financial_entry = model(FinancialEntryModel::class)->find($financial_entry_id);
+        $modifier_id = $financial_entry->modifier_id;
+
+        return $this->mayAllowForDualCurrency($modifier_id);
+    }
+
+    private function mayAllowForDualCurrency(int $modifier_id): bool {
         $modifier = model(ModifierModel::class)->find($modifier_id);
         $accounts = model(AccountModel::class)
             ->whereIn("id", [ $modifier->account_id, $modifier->opposite_account_id ])
