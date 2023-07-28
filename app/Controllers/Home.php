a599547f55e87d1f7c3c2b887d4d26420b723239
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-07-29 04:06:23 +0800

    feat(home): make home return a server info

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,12 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $metadata = json_decode(file_get_contents(ROOTPATH."/public/metadata.json"), true);
+        return response()->setJSON([
+            "data" => [
+                "csrf_token" => csrf_hash()
+            ],
+            "meta" => $metadata
+        ]);
     }
 }
