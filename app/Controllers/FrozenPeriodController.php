360b72b41e5daf182857c998dd9cf8785f3a282c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-26 23:32:01 +0800

    internal(frozen period): create summary calculations after main creation

--- app/Controllers/FrozenPeriodController.php
+++ app/Controllers/FrozenPeriodController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use Brick\Math\BigRational;
 use CodeIgniter\Validation\Validation;
 
 use App\Contracts\OwnedResource;
@@ -43,7 +44,7 @@ class FrozenPeriodController extends BaseOwnedResourceController
         }
 
         $summary_calculations = model(SummaryCalculationModel::class)
-            ->where("id", $initial_document[static::getIndividualName()]->id)
+            ->where("frozen_period_id", $initial_document[static::getIndividualName()]->id)
             ->findAll();
         $enriched_document["summary_calculations"] = $summary_calculations;
 
@@ -78,6 +79,32 @@ class FrozenPeriodController extends BaseOwnedResourceController
         return $enriched_document;
     }
 
+    protected static function processCreatedDocument(array $created_document): array {
+        $main_document = $created_document[static::getIndividualName()];
+
+        $financial_entries = model(FinancialEntryModel::class)
+            ->where("transacted_at >=", $main_document["started_at"])
+            ->where("transacted_at <=", $main_document["finished_at"])
+            ->findAll();
+
+        $raw_summary_calculations = $this->makeRawSummaryCalculations($financial_entries);
+        $raw_summary_calculations = array_map(
+            function ($raw_summary_calculation) use ($main_document) {
+                return array_merge(
+                    [ "frozen_period_id" => $main_document["id"] ],
+                    $raw_summary_calculation
+                );
+            },
+            $raw_summary_calculations
+        );
+        $summary_calculations = model(SummaryCalculationModel::class)
+            ->insertBatch($raw_summary_calculations)
+            ->findAll();
+        $processed_document["summary_calculations"] = $summary_calculations;
+
+        return $created_document;
+    }
+
     private static function makeValidation(): Validation {
         $validation = single_service("validation");
         $individual_name = static::getIndividualName();
@@ -97,5 +124,142 @@ class FrozenPeriodController extends BaseOwnedResourceController
         ]);
 
         return $validation;
+    }
+
+    private static function makeRawSummaryCalculations(array $financial_entries): array {
+        helper("array");
+
+        $linked_modifiers = [];
+        foreach ($financial_entries as $document) {
+            $modifier_id = $document->modifier_id;
+            array_push($linked_modifiers, $modifier_id);
+        }
+
+        $modifiers = [];
+        if (count($linked_modifiers) > 0) {
+            $modifiers = model(ModifierModel::class)
+                ->whereIn("id", array_unique($linked_accounts))
+                ->findAll();
+        }
+
+        $linked_accounts = [];
+        foreach ($modifiers as $document) {
+            $debit_account_id = $document->debit_account_id;
+            $credit_account_id = $document->credit_account_id;
+            array_push($linked_accounts, $debit_account_id, $credit_account_id);
+        }
+
+        $accounts = [];
+        if (count($linked_accounts) > 0) {
+            $accounts = model(AccountModel::class)
+                ->whereIn("id", array_unique($linked_accounts))
+                ->findAll();
+        }
+
+        $linked_currencies = [];
+        foreach ($accounts as $document) {
+            $currency_id = $document->currency_id;
+            array_push($linked_currencies, $currency_id);
+        }
+
+        $currencies = [];
+        if (count($linked_currencies) > 0) {
+            $currencies = model(CurrencyModel::class)
+                ->whereIn("id", array_unique($linked_currencies))
+                ->findAll();
+        }
+
+        $grouped_financial_entries = array_reduce(
+            $financial_entries,
+            function ($groups, $entry) {
+                if (isset($groups[$entry->modifier_id])) {
+                    $groups[$entry->modifier_id] = [];
+                }
+
+                array_push($groups[$entry->modifier_id], $entry);
+
+                return $groups;
+            },
+            []
+        );
+
+        $raw_summary_calculations = array_group_by(
+            array_map(
+                fn ($account) => [
+                    "account_id" => $account->id,
+                    "unadjusted_debit_amount" => BigRational::zero(),
+                    "unadjusted_credit_amount" => BigRational::zero(),
+                    "adjusted_debit_amount" => BigRational::zero(),
+                    "adjusted_credit_amount" => BigRational::zero()
+                ],
+                $accounts
+            ),
+            [ "account_id" ]
+        );
+        $raw_summary_calculations = array_reduce(
+            $modifiers,
+            function ($raw_calculations, $modifier) use ($grouped_financial_entries) {
+                $financial_entries = $grouped_financial_entries[$modifier->id];
+
+                foreach ($financial_entries as $financial_entry) {
+                    $debit_account_id = $modifier->debit_account_id;
+                    $debit_amount = $financial_entry->debit_amount;
+
+                    $credit_account_id = $modifier->credit_account_id;
+                    $credit_amount = $financial_entry->credit_amount;
+
+                    if ($modifier->action !== "close") {
+                        $raw_calculations[$debit_account_id]["unadjusted_debit_amount"]
+                            = $raw_calculations[$debit_account_id]["unadjusted_debit_amount"]
+                                ->plus($debit_amount);
+                        $raw_calculations[$credit_account_id]["unadjusted_credit_amount"]
+                            = $raw_calculations[$credit_account_id]["unadjusted_credit_amount"]
+                                ->plus($credit_amount);
+                    }
+
+                    $raw_calculations[$debit_account_id]["adjusted_debit_amount"]
+                    = $raw_calculations[$debit_account_id]["adjusted_debit_amount"]
+                        ->plus($debit_amount);
+                    $raw_calculations[$credit_account_id]["adjusted_credit_amount"]
+                        = $raw_calculations[$credit_account_id]["adjusted_credit_amount"]
+                            ->plus($credit_amount);
+                }
+
+                return $raw_calculations;
+            },
+            $raw_summary_calculations
+        );
+        $raw_calculations = array_map(
+            function ($raw_calculation) {
+                $unadjusted_debit_amount = $raw_calculation["unadjusted_debit_amount"];
+                $unadjusted_credit_amount = $raw_calculation["unadjusted_credit_amount"];
+                $adjusted_debit_amount = $raw_calculation["adjusted_debit_amount"];
+                $adjusted_credit_amount = $raw_calculation["adjusted_credit_amount"];
+
+                $unadjusted_balance = $unadjusted_debit_amount->minus($unadjusted_credit_amount);
+                $adjusted_balance = $adjusted_debit_amount->minus($adjusted_credit_amount);
+
+                $is_unadjusted_balance_positive = $unadjusted_balance->getSign() > 0;
+                $is_adjusted_balance_positive = $adjusted_balance->getSign() > 0;
+
+                $raw_calculation["unadjusted_debit_amount"] = $is_unadjusted_balance_positive
+                    ? $unadjusted_balance
+                    : BigRational::zero();
+                $raw_calculation["unadjusted_credit_amount"] = $is_unadjusted_balance_positive
+                    ? BigRational::zero()
+                    : $unadjusted_balance->negated();
+                $raw_calculation["adjusted_debit_amount"] = $is_adjusted_balance_positive
+                    ? $adjusted_balance
+                    : BigRational::zero();
+                $raw_calculation["adjusted_credit_amount"] = $is_adjusted_balance_positive
+                    ? BigRational::zero()
+                    : $adjusted_balance->negated();
+
+                return $raw_calculation;
+            },
+            array_values($raw_calculations)
+        );
+
+        return $raw_summary_calculations;
     }
 }
