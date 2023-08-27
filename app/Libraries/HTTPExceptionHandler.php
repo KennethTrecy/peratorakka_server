b1c9009eede38181a25490bbbf5d3bfb2b59b5de
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-08-27 13:05:57 +0800

    internal(library): make custom exception handler

---
+++ app/Libraries/HTTPExceptionHandler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Libraries;
+
+use Throwable;
+
+use CodeIgniter\Debug\BaseExceptionHandler;
+use CodeIgniter\Debug\ExceptionHandlerInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class HTTPExceptionHandler extends BaseExceptionHandler implements ExceptionHandlerInterface
+{
+    public function handle(
+        Throwable $exception,
+        RequestInterface $request,
+        ResponseInterface $response,
+        int $statusCode,
+        int $exitCode
+    ): void {
+        $this->setResponseFormat("json")->respond([
+            "errors" => [
+                [
+                    "message" => $exception->getMessage()
+                ]
+            ]
+        ]);
+
+        exit($exitCode);
+    }
+}
