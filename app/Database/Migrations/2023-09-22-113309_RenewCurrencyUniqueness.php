d0232f1d16e070c764b3b4fab213514421b4aa00
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-22 19:52:41 +0800

    internal(currency): make migration to renew uniqueness of currency
    Closes #6

---
+++ app/Database/Migrations/2023-09-22-113309_RenewCurrencyUniqueness.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class RenewCurrencyUniqueness extends Migration
+{
+    public function up()
+    {
+        $this->forge->dropKey("currencies", "currencies_code_key", false);
+        $this->forge->dropKey("currencies", "currencies_name_key", false);
+        $this->forge->addUniqueKey([ "user_id", "code", "name" ]);
+        $this->forge->processIndexes("currencies");
+    }
+
+    public function down()
+    {
+        $this->forge->dropKey("currencies", "currencies_user_id_code_name", false);
+        $this->forge->addUniqueKey("code", "currencies_code_key");
+        $this->forge->addUniqueKey("name", "currencies_name_key");
+        $this->forge->processIndexes("currencies");
+    }
+}
