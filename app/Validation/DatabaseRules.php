15dbf976336bfca6def48a0568a27dd730dfcdd6
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-09-23 09:02:52 +0800

    internal(validation): extract of extra parameters for uniqueness check

--- app/Validation/DatabaseRules.php
+++ app/Validation/DatabaseRules.php
@@ -73,21 +73,39 @@ class DatabaseRules {
         return true;
     }
 
+    /**
+     * Validator syntax: `is_unique_compositely[
+     *      model:column_name
+     *      |other_column->body_pointer
+     *      |other_column=raw_value,
+     *      optional_field=optional_value
+     * ]`
+     */
     public function is_unique_compositely(
         $value,
         string $parameters,
         array $data,
         ?string &$error = null
     ): bool {
+        helper("array");
+
         $parameters = explode(",", $parameters);
 
-        if (count($parameters) < 2) {
+        if (count($parameters) < 1) {
             $error = 'Number of parameters is fewer than required number'
                 .' in "{0}" to check if the value in {field} is unique.';
             return false;
         }
 
-        $essential_parameters = explode(":", $parameters[0]);
+        $combined_parameters = explode("|", $parameters[0]);
+
+        if (count($combined_parameters) < 2) {
+            $error = 'Number of combined parameters is fewer than required number'
+                .' in "{0}" to check if the value in {field} is unique.';
+            return false;
+        }
+
+        $essential_parameters = explode(":", $combined_parameters[0]);
         if (
             count($essential_parameters) < 2
             || !(model($essential_parameters[0]) instanceof BaseResourceModel)
@@ -96,6 +114,42 @@ class DatabaseRules {
             $error = 'A model name and column name of the existing value is required'
                 .' in "{0}" to check if the value in {field} is unique.';
             return false;
+        }
+
+        $extra_parameters = [];
+        $parameter_count = count($combined_parameters);
+        for ($i = 1; $i < $parameter_count; $i++) {
+            $composite_column_data = $combined_parameters[$i];
+            $position_of_equals = strpos($composite_column_data, "=");
+            $position_of_pointer = strpos($composite_column_data, "->");
+
+            if (
+                (
+                    $position_of_pointer === -1
+                    && $position_of_equals >= 0
+                ) || (
+                    $position_of_equals < $position_of_pointer
+                )
+            ) {
+                $extra_parameter = explode("=", $composite_column_data);
+                array_push($extra_parameters, $extra_parameter);
+            } else if (
+                (
+                    $position_of_equals === -1
+                    && $position_of_pointer >= 0
+                ) || (
+                    $position_of_pointer < $position_of_equals
+                )
+            ) {
+                $extra_parameter = explode("->", $composite_column_data);
+                $extra_parameter[1] = dot_array_search($extra_parameter[1], $data);
+
+                array_push($extra_parameters, $extra_parameter);
+            } else {
+                $error = 'A valid composite data is required'
+                    .' in "{0}" to check if the value in {field} is unique.';
+                return false;
+            }
         }
 
         return true;
