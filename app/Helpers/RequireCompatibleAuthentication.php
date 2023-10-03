01e84b4d62cebb0e19e4ca16991df49d90e85be5
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-10-03 19:48:26 +0800

    internal(helper): make trait to evaluate if request is compatible

---
+++ app/Helpers/RequireCompatibleAuthentication.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Helpers;
+
+use CodeIgniter\HTTP\RequestInterface;
+
+trait RequireCompatibleTokenExpiration {
+    public function hasCompatibleTokenExpirationType(RequestInterface $request): bool {
+        $current_body = $this->request->getJSON(true);
+
+        return
+            isset($current_body["@meta"])
+            && isset($current_body["@meta"]["expiration_type"])
+            && count(
+                array_diff(
+                    SUPPORTED_TOKEN_EXPIRATION_TYPES,
+                    $current_body["@meta"]["expiration_type"]
+                )
+            ) > 0;
+    }
+}
