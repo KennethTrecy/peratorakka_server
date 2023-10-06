97fdd94eace58199649ebf551d51bb9ed4ae87b1
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-10-06 23:42:45 +0800

    fix(helper): correct the expected body

--- app/Helpers/RequireCompatibleTokenExpiration.php
+++ app/Helpers/RequireCompatibleTokenExpiration.php
@@ -6,15 +6,15 @@ use CodeIgniter\HTTP\RequestInterface;
 
 trait RequireCompatibleTokenExpiration {
     public function hasCompatibleTokenExpirationType(RequestInterface $request): bool {
-        $current_body = $this->request->getJSON(true);
+        $current_body = $request->getJSON(true);
 
         return
             isset($current_body["@meta"])
-            && isset($current_body["@meta"]["expiration_type"])
+            && isset($current_body["@meta"]["expiration_types"])
             && count(
                 array_diff(
                     SUPPORTED_TOKEN_EXPIRATION_TYPES,
-                    [ $current_body["@meta"]["expiration_type"] ]
+                    [ ...$current_body["@meta"]["expiration_types"] ]
                 )
             ) < count(SUPPORTED_TOKEN_EXPIRATION_TYPES);
     }
