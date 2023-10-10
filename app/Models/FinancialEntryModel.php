e02edcaab459a903323895476819ff11934f8dcf
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-10-10 21:12:31 +0800

    feat(financial entry): allow entries to be selected by involved accounts

--- app/Models/FinancialEntryModel.php
+++ app/Models/FinancialEntryModel.php
@@ -41,6 +41,26 @@ class FinancialEntryModel extends BaseResourceModel
         ];
     }
 
+    public function filterList(BaseResourceModel $query_builder, array $options) {
+        $query_builder = parent::filterList($query_builder, $options);
+
+        $filter_account_id = $options["account_id"] ?? null;
+
+        if (is_null($filter_account_id)) {
+            return $query_builder;
+        } else {
+            return $query_builder
+                ->whereIn(
+                    "modifier_id",
+                    model(ModifierModel::class, false)
+                        ->builder()
+                        ->select("id")
+                        ->whereIn("debit_account_id", $filter_account_id)
+                        ->orWhereIn("credit_account_id", $filter_account_id)
+                );
+        }
+    }
+
     public function limitSearchToUser(BaseResourceModel $query_builder, User $user) {
         $account_subquery = model(AccountModel::class, false)
             ->builder()
