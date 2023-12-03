6b730c3d7e6266667b9daebe66dbb86cf9f1a2a0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2023-12-03 09:48:15 +0800

    test(feature): add test for updating password of the user

--- t/Feature/Resource/UserTest.php
+++ t/Feature/Resource/UserTest.php
@@ -40,4 +40,33 @@ class UserTest extends AuthenticatedHTTPTestCase
             "secret" => $user_data["email"]
         ]);
     }
+
+    public function testPasswordUpdate()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $faker = Factory::create();
+        $original_new_password = $faker->password();
+        $password_data = [
+            "old_password" => $authenticated_info->getPassword(),
+            "new_password" => $original_new_password,
+            "confirm_new_password" => $original_new_password
+        ];
+
+        $result = $authenticated_info
+            ->getRequest()
+            ->withBodyFormat("json")
+            ->patch("/api/v1/user/password", [
+                "user" => $password_data
+            ]);
+
+        $result->assertStatus(204);
+        $users = model(setting("Auth.userProvider"));
+        $found_user = $users->findByCredentials([
+            "email" => $authenticated_info->getUser()->email,
+            "password" => $original_new_password
+        ]);
+        $this->assertNotNull($found_user);
+        $this->assertSame($found_user->id, $authenticated_info->getUser()->id);
+    }
 }
