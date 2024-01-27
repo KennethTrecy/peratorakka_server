164503d8c61870826103db901d43f2bccfb2144b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-01-27 19:36:41 +0800

    internal(access token): make controller

---
+++ app/Controllers/AccessTokenController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Shield\Entities\User;
+use CodeIgniter\Shield\Models\TokenLoginModel;
+use CodeIgniter\Validation\Validation;
+
+class AccessTokenController extends BaseOwnedResourceController
+{
+    protected static function getIndividualName(): string {
+        return "access_token";
+    }
+
+    protected static function getCollectiveName(): string {
+        return "access_tokens";
+    }
+
+    protected static function getModelName(): string {
+        return TokenLoginModel::class;
+    }
+
+    protected static function makeCreateValidation(User $owner): Validation {
+        return static::makeValidation();
+    }
+
+    protected static function makeUpdateValidation(User $owner, int $resource_id): Validation {
+        return static::makeValidation();
+    }
+
+    private static function makeValidation(): Validation {
+        $validation = single_service("validation");
+
+        return $validation;
+    }
+}
