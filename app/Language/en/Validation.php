f69b7b7e8ed66aa8e20fc25caf087cc2b1593c64
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-29 21:44:24 +0800

    internal(language): add custom validation message for enumeration rules

--- app/Language/en/Validation.php
+++ app/Language/en/Validation.php
@@ -2,7 +2,9 @@
 
 // override core en language system validation or define your own en language validation message
 return [
-    'ensure_ownership' => '{field} must be owned by the current user and present.',
-    'has_column_value_in_list' => '{field} does not match the acceptable values.',
-    'is_unique_compositely' => '{field} must be a unique value in the database.'
+    'may_allow_exchange_action' => 'The "{field}" modifier only allows same values in debit side'
+        .' and credit side.',
+    'ensure_ownership' => 'The {field} must be owned by the current user and present.',
+    'has_column_value_in_list' => 'The {field} does not match the acceptable values.',
+    'is_unique_compositely' => 'The {field} must be a unique value in the database.'
 ];
