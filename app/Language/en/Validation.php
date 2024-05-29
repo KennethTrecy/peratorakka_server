3c25d66ceaf9fd9dfdeffe7dae793b0e7f98de61
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-29 22:06:20 +0800

    internal(validation): separate error messages for time rules

--- app/Language/en/Validation.php
+++ app/Language/en/Validation.php
@@ -8,8 +8,13 @@ return [
         .' debit side and credit side.',
     'must_be_same_as_password_of_current_user' => 'The "{field}" must be same to the current'
         .' password of the current user.',
+
     'may_allow_exchange_action' => 'The "{field}" modifier only allows same values in debit side'
         .' and credit side.',
+
+    'must_be_on_or_before_current_time' => 'The {field} must be on or before the current time.',
+    'must_be_before_incoming_midnight' => 'The {field} must be before the incoming midnight.',
+
     'ensure_ownership' => 'The {field} must be owned by the current user and present.',
     'has_column_value_in_list' => 'The {field} does not match the acceptable values.',
     'is_unique_compositely' => 'The {field} must be a unique value in the database.'
