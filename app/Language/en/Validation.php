301612643a6749105de16c17c7a59722d6017a56
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-29 21:59:38 +0800

    internal(validation): separate error messages for similarity rules

--- app/Language/en/Validation.php
+++ app/Language/en/Validation.php
@@ -2,6 +2,12 @@
 
 // override core en language system validation or define your own en language validation message
 return [
+    'must_be_same_for_modifier' => 'The "{field}" modifier only allows same values in debit side'
+        .' and credit side.',
+    'must_be_same_for_financial_entry' => 'The "{field}" modifier only allows same values in'
+        .' debit side and credit side.',
+    'must_be_same_as_password_of_current_user' => 'The "{field}" must be same to the current'
+        .' password of the current user.',
     'may_allow_exchange_action' => 'The "{field}" modifier only allows same values in debit side'
         .' and credit side.',
     'ensure_ownership' => 'The {field} must be owned by the current user and present.',
