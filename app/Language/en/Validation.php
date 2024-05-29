34683f124fcb9b40730b7afb49d374b928ef06c3
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-05-29 21:24:54 +0800

    internal(validation): separate error messages for database rules

--- app/Language/en/Validation.php
+++ app/Language/en/Validation.php
@@ -1,4 +1,8 @@
 <?php
 
 // override core en language system validation or define your own en language validation message
-return [];
+return [
+    'ensure_ownership' => '{field} must be owned by the current user and present.',
+    'has_column_value_in_list' => '{field} does not match the acceptable values.',
+    'is_unique_compositely' => '{field} must be a unique value in the database.'
+];
