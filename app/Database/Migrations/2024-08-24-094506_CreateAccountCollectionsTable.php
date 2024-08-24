d65ffc10ac2112c5604ef5c67a417473e69ae2ff
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-24 18:41:26 +0800

    internal(account collection): make migration

---
+++ app/Database/Migrations/2024-08-24-094506_CreateAccountCollectionsTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateAccountCollectionsTable extends Migration
+{
+    public function up()
+    {
+        $database = Database::connect();
+
+        $this->forge->addField([
+            "id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+                "auto_increment" => true,
+            ],
+            "collection_id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+            ],
+            "account_id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+            ]
+        ]);
+        $this->forge->addPrimaryKey("id", "pk_account_collections");
+        if ($database->DBDriver !== "SQLite3") {
+            $this->forge->addForeignKey(
+                "collection_id",
+                "collections",
+                "id",
+                "CASCADE",
+                "CASCADE",
+                "account_collections_collection_id_foreign"
+            );
+            $this->forge->addForeignKey(
+                "account_id",
+                "accounts",
+                "id",
+                "CASCADE",
+                "CASCADE",
+                "account_collections_account_id_foreign"
+            );
+        }
+        $this->forge->createTable("account_collections");
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable("account_collections");
+    }
+}
