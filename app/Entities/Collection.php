8bb4354eb8f3ab03d7ec1672b3be475c0e2bd04a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-24 18:49:28 +0800

    internal(collection): add date colums in entity

--- app/Entities/Collection.php
+++ app/Entities/Collection.php
@@ -6,6 +6,12 @@ class Collection extends BaseResourceEntity
 {
     protected $datamap = [];
 
+    protected $dates = [
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    ];
+
     protected $casts = [
         "id" => "integer",
         "user_id" => "integer",
