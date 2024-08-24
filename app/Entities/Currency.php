cea1d9571aaf98b66ef75aacdc980961dea9fe52
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-24 19:38:48 +0800

    fix(currency): specify date fields in entity

--- app/Entities/Currency.php
+++ app/Entities/Currency.php
@@ -6,6 +6,12 @@ class Currency extends BaseResourceEntity
 {
     protected $datamap = [];
 
+    protected $dates = [
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    ];
+
     protected $casts = [
         "id" => "integer",
         "user_id" => "integer",
