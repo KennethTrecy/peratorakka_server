5828536db8f13a40c307df48a3964eea8007b15f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-25 10:59:09 +0800

    test(feature): ensure all account collection routes are working

---
+++ t/Feature/Resource/AccountCollectionTest.php
@@ -0,0 +1,422 @@
+<?php
+
+namespace Tests\Feature\Resource;
+
+use Throwable;
+
+use CodeIgniter\Exceptions\PageNotFoundException;
+use CodeIgniter\Test\Fabricator;
+
+use App\Exceptions\MissingResource;
+use App\Exceptions\InvalidRequest;
+use App\Models\AccountModel;
+use App\Models\CollectionModel;
+use App\Models\CurrencyModel;
+use App\Models\AccountCollectionModel;
+use Tests\Feature\Helper\AuthenticatedHTTPTestCase;
+
+class AccountCollectionTest extends AuthenticatedHTTPTestCase
+{
+    public function testDefaultIndex()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->create();
+
+        $this->expectException(PageNotFoundException::class);
+        $this->expectExceptionCode(404);
+        $result = $authenticated_info->getRequest()->get("/api/v1/account_collections");
+    }
+
+    public function testDefaultShow()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->create();
+
+        $result = $authenticated_info
+            ->getRequest()
+            ->get("/api/v1/account_collections/$account_collection->id");
+
+        $result->assertOk();
+        $result->assertJSONExact([
+            "collections" => json_decode(json_encode([ $collection ])),
+            "accounts" => json_decode(json_encode([ $account ])),
+            "account_collection" => json_decode(json_encode($account_collection))
+        ]);
+    }
+
+    public function testDefaultCreate()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->make();
+
+        $result = $authenticated_info
+            ->getRequest()
+            ->withBodyFormat("json")
+            ->post("/api/v1/account_collections", [
+                "account_collection" => $account_collection->toArray()
+            ]);
+
+        $result->assertOk();
+        $result->assertJSONFragment([
+            "account_collection" => $account_collection->toArray()
+        ]);
+    }
+
+    public function testDefaultUpdate()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->create();
+        $new_details = $account_collection_fabricator->make();
+
+        $this->expectException(PageNotFoundException::class);
+        $this->expectExceptionCode(404);
+        $result = $authenticated_info
+            ->getRequest()
+            ->withBodyFormat("json")
+            ->put("/api/v1/account_collections/$account_collection->id", [
+                "collection" => $new_details->toArray()
+            ]);
+
+        $this->seeInDatabase("account_collections", array_merge(
+            [ "id" => $account_collection->id ],
+            $account_collection->toArray()
+        ));
+    }
+
+    public function testDefaultDelete()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->create();
+
+        $this->expectException(PageNotFoundException::class);
+        $this->expectExceptionCode(404);
+        $result = $authenticated_info
+            ->getRequest()
+            ->delete("/api/v1/account_collections/$collection->id");
+
+        $this->seeInDatabase("account_collections", array_merge(
+            [ "id" => $account_collection->id ]
+        ));
+    }
+
+    public function testDefaultRestore()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->create();
+        model(AccountCollectionModel::class)->delete($account_collection->id);
+
+        $this->expectException(PageNotFoundException::class);
+        $this->expectExceptionCode(404);
+        $result = $authenticated_info
+            ->getRequest()
+            ->patch("/api/v1/account_collections/$account_collection->id");
+
+        $this->dontSeeInDatabase("account_collections", [
+            "id" => $account_collection->id
+        ]);
+    }
+
+    public function testDefaultForceDelete()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->create();
+
+        $result = $authenticated_info
+            ->getRequest()
+            ->delete("/api/v1/account_collections/$collection->id/force");
+
+        $result->assertStatus(204);
+        $this->seeNumRecords(0, "account_collections", []);
+    }
+
+    public function testEmptyIndex()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $this->expectException(PageNotFoundException::class);
+        $this->expectExceptionCode(404);
+        $result = $authenticated_info->getRequest()->get("/api/v1/account_collections");
+    }
+
+    public function testQueriedIndex()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->create();
+
+        $this->expectException(PageNotFoundException::class);
+        $this->expectExceptionCode(404);
+        $result = $authenticated_info->getRequest()->get("/api/v1/account_collections", [
+            "page" => [
+                "limit" => 5
+            ]
+        ]);
+    }
+
+    public function testMissingShow()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id
+        ]);
+        $collection = $collection_fabricator->create();
+        $collection->id = $collection->id + 1;
+
+        $this->expectException(MissingResource::class);
+        $this->expectExceptionCode(404);
+        $result = $authenticated_info->getRequest()->get("/api/v1/account_collections/$collection->id");
+    }
+
+    public function testInvalidCreate()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+        $another_user = $this->makeUser();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $another_user->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->make();
+
+        $this->expectException(InvalidRequest::class);
+        $this->expectExceptionCode(400);
+        $result = $authenticated_info
+            ->getRequest()
+            ->withBodyFormat("json")
+            ->post("/api/v1/account_collections", [
+                "account_collection" => $account_collection->toArray()
+            ]);
+    }
+
+    public function testUnownedDelete()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+        $another_user = $this->makeUser();
+
+        $currency_fabricator = new Fabricator(CurrencyModel::class);
+        $currency_fabricator->setOverrides([
+            "user_id" => $another_user->id
+        ]);
+        $currency = $currency_fabricator->create();
+        $account_fabricator = new Fabricator(AccountModel::class);
+        $account_fabricator->setOverrides([
+            "currency_id" => $currency->id
+        ]);
+        $account = $account_fabricator->create();
+        $collection_fabricator = new Fabricator(CollectionModel::class);
+        $collection_fabricator->setOverrides([
+            "user_id" => $authenticated_info->getUser()->id,
+        ]);
+        $collection = $collection_fabricator->create();
+        $account_collection_fabricator = new Fabricator(AccountCollectionModel::class);
+        $account_collection_fabricator->setOverrides([
+            "collection_id" => $collection->id,
+            "account_id" => $account->id
+        ]);
+        $account_collection = $account_collection_fabricator->create();
+
+        try {
+            $this->expectException(MissingResource::class);
+            $this->expectExceptionCode(404);
+            $result = $authenticated_info
+                ->getRequest()
+                ->delete("/api/v1/account_collections/$account_collection->id/force");
+            $this->assertTrue(false);
+        } catch (MissingResource $error) {
+            $this->seeInDatabase("account_collections", array_merge(
+                [ "id" => $account_collection->id ]
+            ));
+            $this->seeInDatabase("account_collections", [
+                "id" => $account_collection->id
+            ]);
+            throw $error;
+        } catch (Throwable $error) {
+            $this->assertTrue(false);
+        }
+    }
+}
