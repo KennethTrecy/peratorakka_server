ab41e2795cf55ed59756bdf4d0999f08d710f91c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-31 11:26:53 +0800

    internal(library): make expression factory

---
+++ app/Libraries/MathExpression/ExpressionFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Libraries\MathExpression;
+
+use App\Exceptions\ExpressionException;
+use App\Libraries\FlashCache;
+use App\Libraries\MathExpression\ExpressionFactory\RegisterProcedures;
+use App\Libraries\MathExpression\ExpressionFactory\RegisterValues;
+use App\Libraries\MathExpression\PeratorakkaMath;
+use App\Models\AccountCollectionModel;
+use App\Models\CollectionModel;
+use App\Models\FrozenPeriodModel;
+use App\Models\SummaryCalculationModel;
+use Brick\Math\BigRational;
+use CodeIgniter\Database\BaseBuilder;
+use Xylemical\Expressions\ExpressionFactory as BaseExpressionFactory;
+use Xylemical\Expressions\Operator;
+use Xylemical\Expressions\Procedure;
+use Xylemical\Expressions\Token;
+use Xylemical\Expressions\Value;
+
+class ExpressionFactory extends BaseExpressionFactory
+{
+    use RegisterValues;
+    use RegisterProcedures;
+
+    private readonly FlashCache $cache;
+
+    public function __construct(FlashCache $cache, PeratorakkaMath $math) {
+        parent::__construct($math);
+
+        $this->cache = $cache;
+
+        $this->addValues();
+        $this->addProcedures();
+    }
+}
