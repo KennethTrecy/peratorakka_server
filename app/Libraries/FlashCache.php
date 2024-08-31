8d55db4b65a255c8ead992cfa64355369c0d8c02
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-31 11:21:35 +0800

    internal(library): make flash cache to store unserializable values

---
+++ app/Libraries/FlashCache.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Libraries;
+
+class FlashCache
+{
+    private array $cache = [];
+
+    public function store(mixed $value): string {
+        $key = '#'.uniqid();
+
+        $this->cache[$key] = $value;
+
+        return $key;
+    }
+
+    public function flash(string $key, mixed $default = null): mixed {
+        if (!isset($this->cache[$key])) {
+            return $default;
+        }
+
+        $value = $this->cache[$key];
+
+        unset($this->cache[$key]);
+
+        return $value;
+    }
+}
