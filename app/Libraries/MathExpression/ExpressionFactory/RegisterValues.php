373ca7fbdb5ad7478a562866069ae5e9ada7820d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-08-31 11:26:00 +0800

    internal(library): make trait to register expected values in formula

---
+++ app/Libraries/MathExpression/ExpressionFactory/RegisterValues.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Libraries\MathExpression\ExpressionFactory;
+
+use Closure;
+
+use App\Exceptions\ExpressionException;
+use App\Libraries\FlashCache;
+use App\Casts\AccountKind;
+use App\Libraries\MathExpression\Context;
+use App\Models\AccountCollectionModel;
+use App\Models\AccountModel;
+use App\Models\CollectionModel;
+use CodeIgniter\Database\BaseBuilder;
+use Xylemical\Expressions\Token;
+use Xylemical\Expressions\Value;
+
+trait RegisterValues
+{
+    public function addValues() {
+        $this->addValue('COLLECTION\[\d+\]', "evaluateCollection");
+        $this->addValue(
+            '('.join('|', ACCEPTABLE_ACCOUNT_KINDS).')_ACCOUNTS',
+            "evaluateAccountKind"
+        );
+    }
+
+    private function evaluateCollection(array $values, Context $context, Token $token): string {
+        $value = $token->getValue();
+        preg_match('/COLLECTION\[(?P<collection_id>\d+)\]/', $value, $matches);
+        $collection_id = $matches["collection_id"];
+
+        $builder = model(CollectionModel::class, false)
+            ->builder()
+            ->where("id", $collection_id);
+
+        $key = $this->cache->store($builder);
+
+        return $key;
+    }
+
+    private function evaluateAccountKind(array $values, Context $context, Token $token): string {
+        $raw_kind = explode("_", $token->getValue());
+        $kind = strtolower($raw_kind[0]);
+
+        $builder = model(AccountModel::class, false)
+            ->builder()
+            ->where("kind", AccountKind::set($kind));
+
+        $key = $this->cache->store($builder);
+
+        return $key;
+    }
+
+    private function addValue(string $name, string $function_name) {
+        $callback = Closure::fromCallable([ $this, $function_name ]);
+        $this->addOperator(new Value($name, $callback));
+    }
+}
