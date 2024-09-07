fb8da539c6eb3d8313460575ca0f80071d18dc60
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-09-07 15:25:13 +0800

    fix(library): redo the process of math expression

--- app/Libraries/MathExpression.php
+++ app/Libraries/MathExpression.php
@@ -2,11 +2,10 @@
 
 namespace App\Libraries;
 
-use App\Libraries\FlashCache;
 use App\Libraries\MathExpression\Context;
+use App\Libraries\MathExpression\ContextKeys;
 use App\Libraries\MathExpression\ExpressionFactory;
 use App\Libraries\MathExpression\PeratorakkaMath;
-use Brick\Math\BigRational;
 use Xylemical\Expressions\Evaluator;
 use Xylemical\Expressions\Lexer;
 use Xylemical\Expressions\Parser;
@@ -14,28 +13,24 @@ use Xylemical\Expressions\Parser;
 class MathExpression
 {
     private readonly TimeGroupManager $manager;
-    private readonly FlashCache $cache;
 
-    public function __construct(TimeGroupManager $manager) {
+    public function __construct(TimeGroupManager $manager)
+    {
         $this->manager = $manager;
-        $this->cache = new FlashCache();
     }
 
-    public function evaluate(string $formula): array {
+    public function evaluate(string $formula): array
+    {
+        $manager = $this->manager;
+        $cache = $manager->context->getVariable(ContextKeys::CACHE);
         $math = new PeratorakkaMath();
-        $expression_factory = new ExpressionFactory($this->cache, $math);
+        $expression_factory = new ExpressionFactory($cache, $math);
         $lexer = new Lexer($expression_factory);
         $parser = new Parser($lexer);
         $evaluator = new Evaluator();
 
-        $context = new Context($this->manager, $this->cache);
-
-        foreach ($context as $key => $value) {
-            $context->setVariable($key, $value);
-        }
-
         $tokens = $parser->parse($formula);
-        $rawResult = $evaluator->evaluate($tokens, $context);
+        $rawResult = $evaluator->evaluate($tokens, $manager->context);
         $result = $math->resolve($rawResult);
 
         return $result;
