b79c0ea3ea140e43790d4e304d1e4ccb4e58ba9d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-09-07 14:44:44 +0800

    internal(contract): add methods for analyzing date and flow calculations

--- app/Contracts/TimeGroup.php
+++ app/Contracts/TimeGroup.php
@@ -2,13 +2,44 @@
 
 namespace App\Contracts;
 
-use Brick\Math\BigRational;
+use App\Entities\FlowCalculation;
 use App\Entities\SummaryCalculation;
+use Brick\Math\BigRational;
+use CodeIgniter\I18n\Time;
 
+/**
+ * Representation of a data point in a numerical tool.
+ *
+ * All calculations in a time group are considered to be within the same time period and in the same
+ * currencies. Currencies are determined by the numerical tool that owns the instance of time group.
+ */
 interface TimeGroup
 {
     /**
+     * Returns the start date of the time group.
+     *
+     * @return Time
+     */
+    public function startedAt(): Time;
+
+    /**
+     * Returns the finish date of the time group.
+     *
+     * @return Time
+     */
+    public function finishedAt(): Time;
+
+    /**
+     * Returns true if the time group came from unfrozen period, partially or as a whole.
+     *
+     * @return boolean
+     */
+    public function hasSomeUnfrozenDetails(): bool;
+
+    /**
      * Adds summary calculation only if it belongs to the time group.
+     *
+     * Assumes all calculations added are in the same currency.
      *
      * Returns true if it belongs to the group.
      *
@@ -16,6 +47,18 @@ interface TimeGroup
      * @return bool
      */
     public function addSummaryCalculation(SummaryCalculation $summary_calculation): bool;
+
+    /**
+     * Adds flow calculation only if it belongs to the time group.
+     *
+     * Assumes all calculations added are in the same currency.
+     *
+     * Returns true if it belongs to the group.
+     *
+     * @param FlowCalculation $flow_calculation
+     * @return bool
+     */
+    public function addFlowCalculation(FlowCalculation $flow_calculation): bool;
 
     /**
      * @param int[] $selected_account_IDs
