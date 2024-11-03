5f6d1429ea1ce229bb12ca368f3b84c8c765c1ab
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-11-03 19:42:14 +0800

    internal(library): complete the code for exchange rate derivator
    The server code is based from client code. Certain modifications were done.
    
    Reference: https://github.com/KennethTrecy/peratorakka_client/blob/4e65bdba007b7afa90418d3960e774e08f628d4a/src/components/public/utility/derive_exchange_rate.ts

--- app/Libraries/FinancialStatementGroup/ExchangeRateDerivator.php
+++ app/Libraries/FinancialStatementGroup/ExchangeRateDerivator.php
@@ -2,12 +2,165 @@
 
 namespace App\Libraries\FinancialStatementGroup;
 
+use Brick\Math\BigRational;
+
 class ExchangeRateDerivator
 {
-    private readonly array $financial_entries;
+    /**
+     * @var ExchangeRateInfo[]
+     */
+    private readonly array $exchange_rate_infos;
 
-    public function __construct(array $financial_entries)
+    public function __construct(array $exchange_rate_infos)
     {
-        $this->financial_entries = $financial_entries;
+        $this->exchange_rate_infos = array_merge(
+            $exchange_rate_infos,
+            array_map(
+                function (ExchangeRateInfo $exchange_rate_info) {
+                    return $exchange_rate_info->reverse();
+                },
+                $exchange_rate_infos
+            )
+        );
+    }
+
+    public function deriveExchangeRate(
+        int $source_currency_id,
+        int $destination_currency_id
+    ): BigRational {
+        if ($source_currency_id === $destination_currency_id) {
+            return BigRational::of(1);
+        }
+
+        $initial_nodes = array_map(
+            function ($exchange_rate_info) {
+                return [ $exchange_rate_info ];
+            },
+            array_filter(
+                $this->exchange_rate_infos,
+                function (ExchangeRateInfo $exchange_rate_info) use ($source_currency_id) {
+                    return $exchange_rate_info->source_currency_id === $source_currency_id;
+                }
+            )
+        );
+
+        $paths = array_reduce(
+            array_map(
+                function ($path) use ($destination_currency_id) {
+                    return $this->findPaths($destination_currency_id, $path);
+                },
+                $initial_nodes
+            ),
+            function ($previous_paths, $current_path_collection) {
+                return array_merge($previous_paths, $current_path_collection);
+            },
+            []
+        );
+        $light_path = $this->findLightestPath($paths, time());
+        $timestamps = array_map(
+            function (ExchangeRateInfo $exchange_rate_info) {
+                return new \DateTime($exchange_rate_info->updated_at);
+            },
+            $light_path
+        );
+        rsort($timestamps);
+
+        $simplified_value = array_reduce(
+            $light_path,
+            function ($current_value, $current_exchange_rate) {
+                return $current_value->multipliedBy($current_exchange_rate->destination_value)
+                    ->dividedBy($current_exchange_rate->source_value);
+            },
+            BigRational::of(1)
+        )->simplified();
+
+        return $simplified_value;
+    }
+
+
+    public function findPaths(int $destination_currency_id, array $current_path): array {
+        $last_path_index = count($current_path) - 1;
+        $last_exchange_rate = $current_path[$last_path_index];
+        if ($last_exchange_rate->destination_currency_id === $destination_currency_id) {
+            return [ $current_path ];
+        }
+
+        $last_found_currency_IDs = array_reduce(
+            $current_path,
+            function ($previous_IDs, $exchange_rate) {
+                return array_merge($previous_IDs, [
+                    $exchange_rate->source_currency_id,
+                    $exchange_rate->destination_currency_id
+                ]);
+            },
+            []
+        );
+
+        $found_paths = [];
+
+        foreach ($this->exchange_rate_infos as $exchange_rate) {
+            if (
+                $exchange_rate->source_currency_id === $last_exchange_rate->destination_currency_id
+                && !in_array($exchange_rate->destination_currency_id, $last_found_currency_IDs)
+            ) {
+                $new_path = array_merge($current_path, [$exchange_rate]);
+                $found_paths = array_merge(
+                    $found_paths,
+                    $this->findPaths($destination_currency_id, $new_path)
+                );
+            }
+        }
+
+        return $found_paths;
+    }
+
+    private function findLightestPath(array $paths, int $current_time): array {
+        $last_average_weight = INF;
+        $last_path = [];
+
+        foreach ($paths as $path) {
+            $average_weight = $this->weighPath($path, $current_time);
+            if ($average_weight < $last_average_weight) {
+                $last_average_weight = $average_weight;
+                $last_path = $path;
+            }
+
+            if ($average_weight === $last_average_weight) {
+                $timestamps_in_last_path = array_map(
+                    function ($exchange_rate) {
+                        return new \DateTime($exchange_rate->updated_at);
+                    },
+                    $last_path
+                );
+                $timestamps_in_current_path = array_map(
+                    function ($exchange_rate) {
+                        return new \DateTime($exchange_rate->updated_at);
+                    },
+                    $path
+                );
+
+                rsort($timestamps_in_last_path);
+                rsort($timestamps_in_current_path);
+
+                // Get path with latest timestamp
+                if ($timestamps_in_current_path[0] > $timestamps_in_last_path[0]) {
+                    $last_path = $path;
+                }
+            }
+        }
+
+        return $last_path;
+    }
+
+    private function weighPath(array $path, int $current_time): int {
+        $total_weight = 0;
+
+        foreach ($path as $exchange_rate_info) {
+            $last_update = new \DateTime($exchange_rate_info->updated_at);
+            $edge_weight = $current_time - $last_update->getTimestamp();
+            $total_weight += $edge_weight;
+        }
+
+        return $total_weight / count($path);
     }
 }
