9c0c16404c92811595f0ed7f6d05ff4b87f8adf4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-11-03 21:13:24 +0800

    internal(exception): make exception for numerical tool configuration

---
+++ app/Exceptions/NumericalToolConfigurationException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Contracts\APIException;
+use CodeIgniter\Exceptions\ExceptionInterface;
+use CodeIgniter\Exceptions\HTTPExceptionInterface;
+use RuntimeException;
+
+class NumericalToolConfigurationException extends RuntimeException implements ExceptionInterface, HTTPExceptionInterface, APIException
+{
+    use SerializableException;
+
+    public function __construct($message)
+    {
+        parent::__construct($message, 422);
+    }
+}
