48da4711d143fdb09d5e48f6d61813443bc1902c
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-11-09 13:29:35 +0800

    internal(contract): include context in calculation for accuracy

--- app/Contracts/TimeGroup.php
+++ app/Contracts/TimeGroup.php
@@ -4,6 +4,7 @@ namespace App\Contracts;
 
 use App\Entities\FlowCalculation;
 use App\Entities\SummaryCalculation;
+use App\Libraries\Context;
 use Brick\Math\BigRational;
 use CodeIgniter\I18n\Time;
 
@@ -77,38 +78,62 @@ interface TimeGroup
     public function addFlowCalculation(FlowCalculation $flow_calculation): bool;
 
     /**
+     * @param Context $context
      * @param int[] $selected_account_IDs
      * @return BigRational
      */
-    public function totalOpenedDebitAmount(array $selected_account_IDs): BigRational;
+    public function totalOpenedDebitAmount(
+        Context $context,
+        array $selected_account_IDs
+    ): BigRational;
 
     /**
+     * @param Context $context
      * @param int[] $selected_account_IDs
      * @return BigRational
      */
-    public function totalOpenedCreditAmount(array $selected_account_IDs): BigRational;
+    public function totalOpenedCreditAmount(
+        Context $context,
+        array $selected_account_IDs
+    ): BigRational;
 
     /**
+     * @param Context $context
      * @param int[] $selected_account_IDs
      * @return BigRational
      */
-    public function totalUnadjustedDebitAmount(array $selected_account_IDs): BigRational;
+    public function totalUnadjustedDebitAmount(
+        Context $context,
+        array $selected_account_IDs
+    ): BigRational;
 
     /**
+     * @param Context $context
      * @param int[] $selected_account_IDs
      * @return BigRational
      */
-    public function totalUnadjustedCreditAmount(array $selected_account_IDs): BigRational;
+    public function totalUnadjustedCreditAmount(
+        Context $context,
+        array $selected_account_IDs
+    ): BigRational;
 
     /**
+     * @param Context $context
      * @param int[] $selected_account_IDs
      * @return BigRational
      */
-    public function totalClosedDebitAmount(array $selected_account_IDs): BigRational;
+    public function totalClosedDebitAmount(
+        Context $context,
+        array $selected_account_IDs
+    ): BigRational;
 
     /**
+     * @param Context $context
      * @param int[] $selected_account_IDs
      * @return BigRational
      */
-    public function totalClosedCreditAmount(array $selected_account_IDs): BigRational;
+    public function totalClosedCreditAmount(
+        Context $context,
+        array $selected_account_IDs
+    ): BigRational;
 }
