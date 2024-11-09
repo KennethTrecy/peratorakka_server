836fa09258b6ef1fed9c52a9e6acdb19493b62b0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-11-09 17:30:11 +0800

    fix(library): correct type of numerical value

--- app/Libraries/Constellation/Star.php
+++ app/Libraries/Constellation/Star.php
@@ -2,15 +2,17 @@
 
 namespace App\Libraries\Constellation;
 
+use Brick\Math\BigRational;
+
 /**
  * A star is an individual data point. It contains a display value and a numerical value.
  */
 class Star
 {
     public readonly string $display_value;
-    public readonly float $numerical_value;
+    public readonly BigRational $numerical_value;
 
-    public function __construct(string $display_value, float $numerical_value)
+    public function __construct(string $display_value, BigRational $numerical_value)
     {
         $this->display_value = $display_value;
         $this->numerical_value = $numerical_value;
