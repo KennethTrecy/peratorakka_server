33d13dc69e0414f77a1c90eb19bf94f542699d51
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-11-10 13:59:50 +0800

    refacto(cast): rename the cast for formula output format

--- app/Casts/FormulaOutputFormat.php
+++ app/Casts/FormulaOutputFormat.php
@@ -2,7 +2,7 @@
 
 namespace App\Casts;
 
-class OutputFormat extends UnknownableKind
+class FormulaOutputFormat extends UnknownableKind
 {
     protected static array $KINDS = ACCEPTABLE_FORMULA_OUTPUT_FORMATS;
     protected static string $UNKNOWN_KIND = UNKNOWN_FORMULA_OUTPUT_FORMAT;
