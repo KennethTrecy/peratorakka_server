ba7e612362abf7f17bcc754dbf6843baf561c37a
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2024-11-17 15:52:30 +0800

    fix(library): correct the assignment of formula info

--- app/Libraries/NumericalToolConfiguration/FormulaSource.php
+++ app/Libraries/NumericalToolConfiguration/FormulaSource.php
@@ -13,6 +13,7 @@ use App\Libraries\Context\ContextKeys;
 use App\Libraries\Context\TimeGroupManager;
 use App\Libraries\MathExpression;
 use App\Models\FormulaModel;
+use Brick\Math\RoundingMode;
 
 class FormulaSource implements NumericalToolSource
 {
@@ -31,27 +32,28 @@ class FormulaSource implements NumericalToolSource
 
     private static array $formulae = [];
 
-    private readonly Formula $formula;
+    private readonly Formula $formula_info;
 
     public readonly int $formula_id;
 
     private function __construct(int $formula_id) {
-        $this->formula_info_id = $formula_id;
+        $this->formula_id = $formula_id;
 
-        if (!isset(self::$formulae[$formula_id])) {
+        if (!isset(static::$formulae[$formula_id])) {
             $formula = model(FormulaModel::class, false)->find($formula_id);
             if (is_null($formula)) {
                 throw new NumericalToolConfigurationException("Formula $formula_id not found.");
             }
 
-            self::$formulae[$formula_id] = $formula;
-            $this->formula_info = $formula;
+            static::$formulae[$formula_id] = $formula;
         }
+
+        $this->formula_info = static::$formulae[$formula_id];
     }
 
     public function outputFormatCode(): string {
         if ($this->formula_info->output_format === CURRENCY_FORMULA_OUTPUT_FORMAT) {
-            return CURRENCY_FORMULA_OUTPUT_FORMAT."#$this->formula_info->currency_id";
+            return CURRENCY_FORMULA_OUTPUT_FORMAT."#".$this->formula_info->currency_id;
         }
 
         return $this->formula_info->output_format;
@@ -59,7 +61,10 @@ class FormulaSource implements NumericalToolSource
 
     public function calculate(Context $context): array
     {
-        $context->setVariable(ContextKeys::DESTINATION_CURRENCY_ID, $this->currency_id);
+        $context->setVariable(
+            ContextKeys::DESTINATION_CURRENCY_ID,
+            $this->formula_info->currency_id
+        );
         $context->setVariable(
             ContextKeys::EXCHANGE_RATE_BASIS,
             $this->formula_info->exchange_rate_basis
@@ -72,7 +77,7 @@ class FormulaSource implements NumericalToolSource
 
         $math_expression = new MathExpression($time_group_manager);
 
-        $formula_original_output_format = $this->formula_info->output_format;
+        $formula_presentational_precision = $this->formula_info->presentational_precision;
         $totals = $math_expression->evaluate($this->formula_info->formula);
 
         /**
@@ -98,7 +103,7 @@ class FormulaSource implements NumericalToolSource
     public function toArray(): array {
         return [
             "type" => static::sourceType(),
-            "formula_id" => $this->formula_info_id
+            "formula_id" => $this->formula_id
         ];
     }
 }
