b34d2cd76d72cd26b48cb4c44d9fddd6c1b14d2d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-01-19 19:01:53 +0800

    test(feature): update some expected values for time group manager

--- t/Feature/Libraries/TimeGroupManagerTest.php
+++ t/Feature/Libraries/TimeGroupManagerTest.php
@@ -1163,7 +1163,7 @@ class TimeGroupManagerTest extends AuthenticatedHTTPTestCase
         ]);
 
         $this->assertEquals($totals, [
-            [ RationalNumber::get("2750") ]
+            [ RationalNumber::get("2000"), RationalNumber::get("2750") ]
         ]);
     }
 
@@ -1314,7 +1314,7 @@ class TimeGroupManagerTest extends AuthenticatedHTTPTestCase
         ]);
 
         $this->assertEquals($totals, [
-            [ RationalNumber::get("3500") ]
+            [ RationalNumber::get("2000"), RationalNumber::get("3500") ]
         ]);
     }
 }
