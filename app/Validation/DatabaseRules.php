a324e165950dcb4a6c1bc7bc05ecc0964df23e00
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-02-01 22:39:32 +0800

    internal(validation): add new rules to examine modifier input thoroughly

--- app/Validation/DatabaseRules.php
+++ app/Validation/DatabaseRules.php
@@ -3,6 +3,7 @@
 namespace App\Validation;
 
 use App\Contracts\OwnedResource;
+use App\Libraries\ModifierAtomInputExaminer;
 use App\Models\BaseResourceModel;
 use InvalidArgumentException;
 
@@ -247,5 +248,18 @@ class DatabaseRules
         }
 
         return true;
+    }
+
+    // !: Validate data with `must_have_compound_data_key` first before putting this validator.
+    public function does_own_resources_declared_in_atom_group_info(
+        $value,
+        string $parameters,
+        array $data,
+        ?string &$error = null
+    ): bool {
+        $parameters = explode(",", $parameters);
+        $modifier_atom_input_examiner = ModifierAtomInputExaminer::make($parameters[0], $data);
+
+        return $modifier_atom_input_examiner->validateOwnership();
     }
 }
