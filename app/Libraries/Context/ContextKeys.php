f8195c7b4c64b040bf53da87a56319a13ca823b6
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-02-02 00:49:22 +0800

    internal(library): add new key for modifier cache

--- app/Libraries/Context/ContextKeys.php
+++ app/Libraries/Context/ContextKeys.php
@@ -15,6 +15,7 @@ enum ContextKeys: string
     case FLASH_CACHE = "flash_cache";
     case MEMOIZER = "memoizer";
     case CURRENCY_CACHE = "currency_cache";
+    case MODIFIER_CACHE = "modifier_cache";
     case ACCOUNT_CACHE = "account_cache";
     case COLLECTION_CACHE = "collection_cache";
     case EXCHANGE_RATE_CACHE = "exchange_rate_cache";
