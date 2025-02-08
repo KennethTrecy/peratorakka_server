35c6fbbacd23b0aa2813246d8b9a4dbbbbc931a4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-02-09 02:09:33 +0800

    test(helper): make base test case for those with contexts

---
+++ t/Feature/Helper/AuthenticatedContextualHTTPTestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature\Helper;
+
+use App\Libraries\Context;
+use App\Libraries\FinancialEntryAtomInputExaminer;
+use App\Libraries\ModifierAtomInputExaminer;
+use CodeIgniter\Shield\Entities\User;
+use CodeIgniter\Shield\Test\AuthenticationTesting;
+use Faker\Factory;
+
+class AuthenticatedContextualHTTPTestCase extends AuthenticatedHTTPTestCase
+{
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        Context::clear();
+        FinancialEntryAtomInputExaminer::clear();
+        ModifierAtomInputExaminer::clear();
+    }
+}
