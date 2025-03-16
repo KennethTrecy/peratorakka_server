9bc0ef0f45c1d52038f51b128ce8f0118d910fa4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-03-16 13:19:02 +0800

    internal(collection): renew link model and deprecate old link model

--- app/Models/AccountCollectionModel.php
+++ app/Models/AccountCollectionModel.php
@@ -9,7 +9,7 @@ use Faker\Generator;
 
 class AccountCollectionModel extends BaseResourceModel
 {
-    protected $table = "account_collections";
+    protected $table = "account_collections_v2";
     protected $returnType = AccountCollection::class;
     protected $allowedFields = [
         "collection_id",
@@ -47,8 +47,44 @@ class AccountCollectionModel extends BaseResourceModel
                         model(CurrencyModel::class, false)
                             ->builder()
                             ->select("id")
-                            ->where("user_id", $user->id)
+                            ->whereIn(
+                                "precision_format_id",
+                                model(PrecisionFormatModel::class, false)
+                                    ->builder()
+                                    ->select("id")
+                                    ->where("user_id", $user->id)
+                            )
                     )
             );
+    }
+
+    protected static function createAncestorResources(int $user_id, array $options): array
+    {
+        [
+            $collection
+        ] = $options["collection_parent"] ?? CollectionModel::createTestResource(
+            $user_id,
+            $options["collection_options"] ?? []
+        );
+
+        $account_options = $options["account_options"] ?? [ "expected_kinds" => [] ];
+
+        [
+            $precision_formats,
+            $currencies,
+            $accounts
+        ] = isset($options["ancestor_accounts"])
+            ? $options["ancestor_accounts"]
+            : AccountModel::createTestResources($user_id, 1, $account_options);
+
+        $parent_links = static::permutateParentLinks([
+            "collection_id" => [ $collection->id ],
+            "account_id" => array_map(fn ($account) => $account->id, $accounts)
+        ], $options);
+
+        return [
+            [ $precision_formats, $currencies, $accounts, [ $collection ] ],
+            $parent_links
+        ];
     }
 }
