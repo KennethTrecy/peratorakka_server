218064443194cdf68144a2c575981cca43e00668
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-04-13 15:51:14 +0800

    internal(library)!: update the operations of granular time group

--- app/Libraries/TimeGroup/GranularTimeGroup.php
+++ app/Libraries/TimeGroup/GranularTimeGroup.php
@@ -4,8 +4,9 @@ namespace App\Libraries\TimeGroup;
 
 use App\Casts\RationalNumber;
 use App\Contracts\TimeGroup;
-use App\Entities\FlowCalculation;
-use App\Entities\SummaryCalculation;
+use App\Entities\RealFlowCalculation;
+use App\Entities\RealAdjustedSummaryCalculation;
+use App\Entities\RealUnadjustedSummaryCalculation;
 use App\Libraries\Context;
 use App\Libraries\Context\ContextKeys;
 use Brick\Math\BigRational;
@@ -18,12 +19,17 @@ use Brick\Math\BigRational;
 abstract class GranularTimeGroup implements TimeGroup
 {
     /**
-     * @var SummaryCalculation[]
+     * @var RealAdjustedSummaryCalculation[]
      */
-    protected array $summary_calculations = [];
+    protected array $real_adjusted_summary_calculations = [];
 
     /**
-     * @var FlowCalculation[][]
+     * @var RealUnadjustedSummaryCalculation[]
+     */
+    protected array $real_unadjusted_summary_calculations = [];
+
+    /**
+     * @var RealFlowCalculation[][]
      */
     protected array $flow_calculations = [];
 
@@ -41,104 +47,148 @@ abstract class GranularTimeGroup implements TimeGroup
         return $finished_date->toLocalizedString("MMMM yyyy");
     }
 
-    public function totalOpenedDebitAmount(
+    public function totalRealOpenedDebitAmount(
+        Context $context,
+        array $selected_account_IDs
+    ): array {
+        [
+            $debit_summary_calculations,
+            $credit_summary_calculations
+        ] = $this->selectRealAdjustedSummaryCalculations($context, $selected_account_IDs);
+
+        return [
+            array_reduce(
+                $credit_summary_calculations,
+                function ($total, $summary_calculation) {
+                    return $total->minus($summary_calculation->opened_amount);
+                },
+                array_reduce(
+                    $debit_summary_calculations,
+                    function ($total, $summary_calculation) {
+                        return $total->reduce($summary_calculation->opened_amount);
+                    },
+                    RationalNumber::zero()
+                )
+            )
+        ];
+    }
+
+    public function totalRealOpenedCreditAmount(
+        Context $context,
+        array $selected_account_IDs
+    ): array {
+        [
+            $debit_summary_calculations,
+            $credit_summary_calculations
+        ] = $this->selectRealAdjustedSummaryCalculations($context, $selected_account_IDs);
+
+        return [
+            array_reduce(
+                $debit_summary_calculations,
+                function ($total, $summary_calculation) {
+                    return $total->minus($summary_calculation->opened_amount);
+                },
+                array_reduce(
+                    $credit_summary_calculations,
+                    function ($total, $summary_calculation) {
+                        return $total->reduce($summary_calculation->opened_amount);
+                    },
+                    RationalNumber::zero()
+                )
+            )
+        ];
+    }
+
+    public function totalRealUnadjustedDebitAmount(
         Context $context,
         array $selected_account_IDs
     ): array {
         return [
             array_reduce(
-                $this->selectSummaryCalculations($selected_account_IDs),
+                $this->selectRealUnadjustedSummaryCalculations($selected_account_IDs),
                 function ($total, $summary_calculation) {
-                    return $total->plus($summary_calculation->opened_debit_amount);
+                    return $total->plus($summary_calculation->debit_amount);
                 },
                 RationalNumber::zero()
             )
         ];
     }
 
-    public function totalOpenedCreditAmount(
+    public function totalRealUnadjustedCreditAmount(
         Context $context,
         array $selected_account_IDs
     ): array {
         return [
             array_reduce(
-                $this->selectSummaryCalculations($selected_account_IDs),
+                $this->selectRealUnadjustedSummaryCalculations($selected_account_IDs),
                 function ($total, $summary_calculation) {
-                    return $total->plus($summary_calculation->opened_credit_amount);
+                    return $total->plus($summary_calculation->credit_amount);
                 },
                 RationalNumber::zero()
             )
         ];
     }
 
-    public function totalUnadjustedDebitAmount(
+    public function totalRealClosedDebitAmount(
         Context $context,
         array $selected_account_IDs
     ): array {
+        [
+            $debit_summary_calculations,
+            $credit_summary_calculations
+        ] = $this->selectRealAdjustedSummaryCalculations($context, $selected_account_IDs);
+
         return [
             array_reduce(
-                $this->selectSummaryCalculations($selected_account_IDs),
+                $credit_summary_calculations,
                 function ($total, $summary_calculation) {
-                    return $total->plus($summary_calculation->unadjusted_debit_amount);
+                    return $total->minus($summary_calculation->closed_amount);
                 },
-                RationalNumber::zero()
+                array_reduce(
+                    $debit_summary_calculations,
+                    function ($total, $summary_calculation) {
+                        return $total->reduce($summary_calculation->closed_amount);
+                    },
+                    RationalNumber::zero()
+                )
             )
         ];
     }
 
-    public function totalUnadjustedCreditAmount(
+    public function totalRealClosedCreditAmount(
         Context $context,
         array $selected_account_IDs
     ): array {
+        [
+            $debit_summary_calculations,
+            $credit_summary_calculations
+        ] = $this->selectRealAdjustedSummaryCalculations($context, $selected_account_IDs);
+
         return [
             array_reduce(
-                $this->selectSummaryCalculations($selected_account_IDs),
+                $debit_summary_calculations,
                 function ($total, $summary_calculation) {
-                    return $total->plus($summary_calculation->unadjusted_credit_amount);
+                    return $total->minus($summary_calculation->closed_amount);
                 },
-                RationalNumber::zero()
+                array_reduce(
+                    $credit_summary_calculations,
+                    function ($total, $summary_calculation) {
+                        return $total->reduce($summary_calculation->closed_amount);
+                    },
+                    RationalNumber::zero()
+                )
             )
         ];
     }
 
-    public function totalClosedDebitAmount(
-        Context $context,
-        array $selected_account_IDs
-    ): array {
-        return [
-            array_reduce(
-                $this->selectSummaryCalculations($selected_account_IDs),
-                function ($total, $summary_calculation) {
-                    return $total->plus($summary_calculation->closed_debit_amount);
-                },
-                RationalNumber::zero()
-            )
-        ];
-    }
-
-    public function totalClosedCreditAmount(
-        Context $context,
-        array $selected_account_IDs
-    ): array {
-        return [
-            array_reduce(
-                $this->selectSummaryCalculations($selected_account_IDs),
-                function ($total, $summary_calculation) {
-                    return $total->plus($summary_calculation->closed_credit_amount);
-                },
-                RationalNumber::zero()
-            )
-        ];
-    }
-
-    public function totalNetCashFlowAmount(
+    public function totalRealNetCashFlowAmount(
         Context $context,
         array $cash_flow_activity_IDs,
         array $selected_account_IDs
     ): array {
         return [
             array_reduce(
-                $this->selectFlowCalculations($cash_flow_activity_IDs, $selected_account_IDs),
+                $this->selectRealFlowCalculations($cash_flow_activity_IDs, $selected_account_IDs),
                 function ($total, $flow_calculation) {
                     return $total->plus($flow_calculation->net_amount);
                 },
@@ -147,15 +197,44 @@ abstract class GranularTimeGroup implements TimeGroup
         ];
     }
 
-    private function selectSummaryCalculations(array $selected_account_IDs): array
+    private function selectRealAdjustedSummaryCalculations(
+        Context $context,
+        array $selected_account_IDs
+    ): array {
+        $account_cache = $context->getVariable(ContextKeys::ACCOUNT_CACHE, null);
+        $summary_calculations = $this->real_adjusted_summary_calculations;
+
+        $raw_debit_summary_calculations = [];
+        $raw_credit_summary_calculations = [];
+        foreach ($selected_account_IDs as $account_id) {
+            if (isset($summary_calculations[$account_id])) {
+                $account_kind = $account_cache->determineAccountKind($account_id);
+                if (in_array($account_kind, NORMAL_DEBIT_ACCOUNT_KINDS)) {
+                    array_push(
+                        $raw_debit_summary_calculations,
+                        $summary_calculations[$account_id]
+                    );
+                } else {
+                    array_push(
+                        $raw_credit_summary_calculations,
+                        $summary_calculations[$account_id]
+                    );
+                }
+            }
+        }
+
+        return [ $raw_debit_summary_calculations, $raw_credit_summary_calculations ];
+    }
+
+    private function selectRealUnajustedSummaryCalculations(array $selected_account_IDs): array
     {
-        $summary_calculations = $this->summary_calculations;
+        $real_unadjusted_summary_calculations = $this->real_unadjusted_summary_calculations;
 
         $raw_summary_calculations = array_map(
-            function ($account_id) use ($summary_calculations) {
+            function ($account_id) use ($real_unadjusted_summary_calculations) {
                 // If summary calculation is not found because it does not exist yet during this
                 // period, return null.
-                return $summary_calculations[$account_id] ?? null;
+                return $real_unadjusted_summary_calculations[$account_id] ?? null;
             },
             $selected_account_IDs
         );
@@ -170,16 +249,16 @@ abstract class GranularTimeGroup implements TimeGroup
         return $loaded_summary_calculations;
     }
 
-    private function selectFlowCalculations(
+    private function selectRealFlowCalculations(
         array $cash_flow_activity_IDs,
         array $selected_account_IDs
     ): array {
-        $raw_flow_calculations = [];
+        $raw_real_flow_calculations = [];
 
         foreach ($this->flow_calculations as $cash_flow_activity_id => $flow_calculations) {
             if (in_array($cash_flow_activity_id, $cash_flow_activity_IDs)) {
-                $raw_flow_calculations = [
-                    ...$raw_flow_calculations,
+                $raw_real_flow_calculations = [
+                    ...$raw_real_flow_calculations,
                     ...array_map(
                         function ($account_id) use ($flow_calculations) {
                             // If flow calculation is not found because it does not exist yet during this
@@ -193,7 +272,7 @@ abstract class GranularTimeGroup implements TimeGroup
         }
 
         $loaded_flow_calculations = array_filter(
-            $raw_flow_calculations,
+            $raw_real_flow_calculations,
             function ($flow_calculation) {
                 return $flow_calculation !== null;
             }
