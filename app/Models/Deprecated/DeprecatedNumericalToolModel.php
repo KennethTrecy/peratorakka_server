95cd27db407657eda17ba139a340780cfbacbe53
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-05-31 13:55:25 +0800

    internal(numerical tool): upgrade model

--- app/Models/Deprecated/DeprecatedNumericalToolModel.php
+++ app/Models/Deprecated/DeprecatedNumericalToolModel.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Deprecated;
 
-use App\Entities\NumericalTool;
-use App\Libraries\Resource;
+use App\Entities\Deprecated\NumericalTool;
+use App\Models\BaseResourceModel;
 use App\Libraries\Context;
-use App\Libraries\Context\ContextKeys;
-use App\Libraries\NumericalToolConfiguration\CollectionSource;
+use App\Libraries\NumericalToolConfiguration\Deprecated\DeprecatedCollectionSource;
+use App\Libraries\Resource;
 use App\Libraries\TimeGroup\PeriodicTimeGroup;
 use App\Libraries\TimeGroup\UnfrozenTimeGroup;
 use App\Libraries\TimeGroup\YearlyTimeGroup;
@@ -15,13 +15,12 @@ use CodeIgniter\I18n\Time;
 use CodeIgniter\Shield\Entities\User;
 use Faker\Generator;
 
-class NumericalToolModel extends BaseResourceModel
+class DeprecatedNumericalToolModel extends BaseResourceModel
 {
-    protected $table = "numerical_tools_v2";
+    protected $table = "numerical_tools";
     protected $returnType = NumericalTool::class;
     protected $allowedFields = [
-        "currency_id",
-        "exchange_rate_basis",
+        "user_id",
         "name",
         "kind",
         "recurrence",
@@ -29,7 +28,6 @@ class NumericalToolModel extends BaseResourceModel
         "order",
         "notes",
         "configuration",
-        "created_at",
         "deleted_at"
     ];
 
@@ -46,7 +44,6 @@ class NumericalToolModel extends BaseResourceModel
         return [
             "name"  => $faker->unique()->firstName(),
             "kind"  => $faker->randomElement(ACCEPTABLE_NUMERICAL_TOOL_KINDS),
-            "exchange_rate_basis" => PERIODIC_EXCHANGE_RATE_BASIS,
             "recurrence"  => $faker->randomElement(ACCEPTABLE_NUMERICAL_TOOL_RECURRENCE_PERIODS),
             "recency"  => $faker->numberBetween(-100, 100),
             "order"  => $faker->numberBetween(0, 100),
@@ -56,6 +53,8 @@ class NumericalToolModel extends BaseResourceModel
                     [
                         "type" => CollectionSource::sourceType(),
                         "collection_id" => 1,
+                        "currency_id" => 1,
+                        "exchange_rate_basis" => PERIODIC_EXCHANGE_RATE_BASIS,
                         "stage_basis" => UNADJUSTED_AMOUNT_STAGE_BASIS,
                         "side_basis" => NET_DEBIT_AMOUNT_SIDE_BASIS,
                         "must_show_individual_amounts" => true,
@@ -69,59 +68,19 @@ class NumericalToolModel extends BaseResourceModel
 
     public function limitSearchToUser(BaseResourceModel $query_builder, User $user)
     {
-        return $query_builder
-            ->whereIn(
-                "currency_id",
-                model(CurrencyModel::class, false)
-                    ->builder()
-                    ->select("id")
-                    ->whereIn(
-                        "precision_format_id",
-                        model(PrecisionFormatModel::class, false)
-                            ->builder()
-                            ->select("id")
-                            ->where("user_id", $user->id)
-                    )
-            );
+        return $query_builder->where("user_id", $user->id);
     }
 
     public static function showConstellations(NumericalTool $tool): array
     {
         $context = new Context();
-        $context->setVariable(ContextKeys::DESTINATION_CURRENCY_ID, $tool->currency_id);
-        $context->setVariable(ContextKeys::EXCHANGE_RATE_BASIS, $tool->exchange_rate_basis);
-        $raw_time_groups = static::makeTimeGroups($tool->recurrence, $tool->recency);
-        $time_groups = new TimeGroupManager($context, $raw_time_groups);
+        $time_groups = new TimeGroupManager(
+            $context,
+            static::makeTimeGroups($tool->recurrence, $tool->recency)
+        );
         $constellations = $tool->configuration->calculate($context);
 
         return [ $time_groups->timeTags(), $constellations ];
-    }
-
-    protected static function createAncestorResources(int $user_id, array $options): array
-    {
-        [
-            $precision_formats,
-            $currency
-        ] = $options["parents"] ?? CurrencyModel::createTestResource(
-            $user_id,
-            $options["currency_options"] ?? []
-        );
-
-        $parent_links = static::permutateParentLinks([
-            "currency_id" => [ $currency->id ]
-        ], $options);
-
-        return [
-            [ $precision_formats, [ $currency ] ],
-            $parent_links
-        ];
-    }
-
-    protected static function identifyAncestors(): array
-    {
-        return [
-            CurrencyModel::class => [ "currency_id" ]
-        ];
     }
 
     private static function makeTimeGroups(string $recurrence, int $recency): array
@@ -167,18 +126,11 @@ class NumericalToolModel extends BaseResourceModel
             ->setHour(0)->setMinute(0)->setSecond(0);
         $frozen_time_group_limit = abs($recency);
 
-        if ($must_include_unfrozen_period) {
-            if ($maxed_current_date->isAfter($possible_unfrozen_date)) {
-                array_push($time_groups, UnfrozenTimeGroup::make(
-                    $possible_unfrozen_date,
-                    $maxed_current_date
-                ));
-            } else {
-                // Sometimes, max current date is less than or equal to possible unfrozen date.
-                // This situation happens when all possible time periods are frozen.
-                // Treat the time group as frozen instead.
-                array_push($time_groups, new PeriodicTimeGroup($last_frozen_period));
-            }
+        if ($must_include_unfrozen_period && $current_date->isAfter($possible_unfrozen_date)) {
+            array_push($time_groups, UnfrozenTimeGroup::make(
+                $possible_unfrozen_date,
+                $maxed_current_date
+            ));
 
             $latest_known_date = $maxed_current_date;
         }
