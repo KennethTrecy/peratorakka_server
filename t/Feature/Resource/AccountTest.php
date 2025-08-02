f706c43ca9d0761c0286a5681afae7e36462b83e
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-08-02 15:32:59 +0800

    test(feature): ensure account API can create itemized asset

--- t/Feature/Resource/AccountTest.php
+++ t/Feature/Resource/AccountTest.php
@@ -2,16 +2,19 @@
 
 namespace Tests\Feature\Resource;
 
+use App\Casts\AccountKind;
 use App\Exceptions\InvalidRequest;
 use App\Exceptions\MissingResource;
 use App\Models\AccountModel;
 use App\Models\CurrencyModel;
+use App\Models\ItemDetailModel;
 use App\Models\PrecisionFormatModel;
-use CodeIgniter\Test\Fabricator;
-use Tests\Feature\Helper\AuthenticatedHTTPTestCase;
+use App\Models\FrozenPeriodModel;
+use CodeIgniter\I18n\Time;
+use Tests\Feature\Helper\AuthenticatedContextualHTTPTestCase;
 use Throwable;
 
-class AccountTest extends AuthenticatedHTTPTestCase
+class AccountTest extends AuthenticatedContextualHTTPTestCase
 {
     public function testDefaultIndex()
     {
@@ -64,7 +67,11 @@ class AccountTest extends AuthenticatedHTTPTestCase
             $precision_formats,
             $currencies,
             $details
-        ] = AccountModel::makeTestResource($authenticated_info->getUser()->id, []);
+        ] = AccountModel::makeTestResource($authenticated_info->getUser()->id, [
+            "overrides" => [
+                "kind" => GENERAL_ASSET_ACCOUNT_KIND
+            ]
+        ]);
 
         $result = $authenticated_info
             ->getRequest()
@@ -90,20 +97,27 @@ class AccountTest extends AuthenticatedHTTPTestCase
             $new_details
         ] = AccountModel::createAndMakeTestResources(
             $authenticated_info->getUser()->id,
-            []
+            [
+                "overrides" => [
+                    "kind" => GENERAL_ASSET_ACCOUNT_KIND
+                ]
+            ]
         );
 
         $result = $authenticated_info
             ->getRequest()
             ->withBodyFormat("json")
             ->put("/api/v2/accounts/$details->id", [
-                "account" => $new_details->toArray()
+                "account" => array_merge($new_details->toArray(), [
+                    "kind" => GENERAL_EXPENSE_ACCOUNT_KIND
+                ])
             ]);
 
         $result->assertStatus(204);
         $this->seeInDatabase("accounts_v2", array_merge(
             [ "id" => $details->id ],
-            $new_details->toRawArray()
+            $new_details->toRawArray(),
+            [ "kind" => AccountKind::set(GENERAL_EXPENSE_ACCOUNT_KIND) ]
         ));
     }
 
@@ -262,6 +276,113 @@ class AccountTest extends AuthenticatedHTTPTestCase
             ]);
     }
 
+    public function testIncompleteCreate()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        [
+            $precision_formats,
+            $currencies,
+            $details
+        ] = AccountModel::makeTestResource($authenticated_info->getUser()->id, [
+            "overrides" => [
+                "kind" => ITEMIZED_ASSET_ACCOUNT_KIND
+            ]
+        ]);
+
+        $this->expectException(InvalidRequest::class);
+        $this->expectExceptionCode(400);
+        $result = $authenticated_info
+            ->getRequest()
+            ->withBodyFormat("json")
+            ->post("/api/v2/accounts", [
+                "account" => $details->toArray()
+            ]);
+    }
+
+    public function testMalformedCompositeCreate()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        [
+            $precision_formats,
+            $currencies,
+            $details
+        ] = AccountModel::makeTestResource($authenticated_info->getUser()->id, [
+            "overrides" => [
+                "kind" => ITEMIZED_ASSET_ACCOUNT_KIND
+            ]
+        ]);
+
+        [
+            $precision_formats,
+            $item_detail_info
+        ] = ItemDetailModel::createTestResource($authenticated_info->getUser()->id, [
+            "precision_format_parent" => $precision_formats
+        ]);
+
+        $this->expectException(InvalidRequest::class);
+        $this->expectExceptionCode(400);
+        $result = $authenticated_info
+            ->getRequest()
+            ->withBodyFormat("json")
+            ->post("/api/v2/accounts", [
+                "account" => array_merge($details->toArray(), [
+                    "@relationship" => [
+                        "item_configuration" => [
+                            "item_detail_id" => $item_detail_info->id,
+                            "valuation_method" => UNKNOWN_VALUATION_METHOD
+                        ]
+                    ]
+                ])
+            ]);
+    }
+
+    public function testCorrectCompositeCreate()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        [
+            $precision_formats,
+            $currencies,
+            $details
+        ] = AccountModel::makeTestResource($authenticated_info->getUser()->id, [
+            "overrides" => [
+                "kind" => ITEMIZED_ASSET_ACCOUNT_KIND
+            ]
+        ]);
+
+        [
+            $precision_formats,
+            $item_detail_info
+        ] = ItemDetailModel::createTestResource($authenticated_info->getUser()->id, [
+            "precision_format_parent" => $precision_formats
+        ]);
+
+        $result = $authenticated_info
+            ->getRequest()
+            ->withBodyFormat("json")
+            ->post("/api/v2/accounts", [
+                "account" => array_merge($details->toArray(), [
+                    "@relationship" => [
+                        "item_configuration" => [
+                            "item_detail_id" => $item_detail_info->id,
+                            "valuation_method" => WEIGHTED_AVERAGE_VALUATION_METHOD
+                        ]
+                    ]
+                ])
+            ]);
+
+        $result->assertOk();
+        $result->assertJSONFragment([
+            "account" => $details->toArray(),
+            "item_configuration" => [
+                "item_detail_id" => $item_detail_info->id,
+                "valuation_method" => WEIGHTED_AVERAGE_VALUATION_METHOD
+            ]
+        ]);
+    }
+
     public function testInvalidUpdate()
     {
         $authenticated_info = $this->makeAuthenticatedInfo();
@@ -286,6 +407,100 @@ class AccountTest extends AuthenticatedHTTPTestCase
             ->getRequest()
             ->withBodyFormat("json")
             ->put("/api/v2/accounts/$details->id", [
+                "account" => $new_details->toArray()
+            ]);
+    }
+
+    public function testProhibitedUpdate()
+    {
+        $authenticated_info = $this->makeAuthenticatedInfo();
+
+        [
+            $precision_formats,
+            $cash_flow_activities,
+            $currencies,
+            $accounts,
+            $frozen_periods,
+            $frozen_accounts,
+            $real_adjusted_summaries,
+            $real_unadjusted_summaries,
+            $real_flows
+        ] = FrozenPeriodModel::createTestPeriods(
+            $authenticated_info->getUser(),
+            [
+                [
+                    "started_at" => Time::now()->subDays(1),
+                    "finished_at" => Time::now(),
+                    "entries" => [
+                        [
+                            "modifier_index" => 0,
+                            "atoms" => [
+                                [ 0, "1000" ],
+                                [ 1, "1000" ]
+                            ]
+                        ],
+                        [
+                            "modifier_index" => 1,
+                            "atoms" => [
+                                [ 2, "250" ],
+                                [ 3, "250" ]
+                            ]
+                        ],
+                        [
+                            "modifier_index" => 2,
+                            "atoms" => [
+                                [ 4, "250" ],
+                                [ 5, "250" ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            [
+                "currency_count" => 1,
+                "cash_flow_activity_count" => 1,
+                "expected_modifier_actions" => [
+                    RECORD_MODIFIER_ACTION,
+                    RECORD_MODIFIER_ACTION,
+                    CLOSE_MODIFIER_ACTION
+                ],
+                "account_combinations" => [
+                    [ 0, EQUITY_ACCOUNT_KIND ],
+                    [ 0, LIQUID_ASSET_ACCOUNT_KIND ],
+                    [ 0, GENERAL_EXPENSE_ACCOUNT_KIND ]
+                ],
+                "modifier_atom_combinations" => [
+                    [ 0, 1, REAL_DEBIT_MODIFIER_ATOM_KIND ],
+                    [ 0, 0, REAL_CREDIT_MODIFIER_ATOM_KIND ],
+                    [ 1, 2, REAL_DEBIT_MODIFIER_ATOM_KIND ],
+                    [ 1, 1, REAL_CREDIT_MODIFIER_ATOM_KIND ],
+                    [ 2, 0, REAL_DEBIT_MODIFIER_ATOM_KIND ],
+                    [ 2, 2, REAL_CREDIT_MODIFIER_ATOM_KIND ]
+                ],
+                "modifier_atom_activity_combinations" => [
+                    [ 1, 0 ],
+                    [ 2, 0 ]
+                ]
+            ]
+        );
+        $equity_account = $accounts[0];
+        [
+            $precision_formats,
+            $currencies,
+            $new_details
+        ] = AccountModel::makeTestResource($authenticated_info->getUser()->id, [
+            "ancestor_currency" => [ $precision_formats, $currencies[0] ],
+            "overrides" => [
+                "kind" => GENERAL_ASSET_ACCOUNT_KIND
+            ]
+        ]);
+
+        $this->expectException(InvalidRequest::class);
+        $this->expectExceptionCode(400);
+        $result = $authenticated_info
+            ->getRequest()
+            ->withBodyFormat("json")
+            ->put("/api/v2/accounts/$equity_account->id", [
                 "account" => $new_details->toArray()
             ]);
     }
