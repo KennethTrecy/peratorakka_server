0002ea689e611891c9f9329315d2a00e86c77f6d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-08-02 12:28:24 +0800

    internal(frozen period): make model for item calculation

---
+++ app/Models/ItemCalculationModel.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Models;
+
+use App\Entities\ItemCalculation;
+use CodeIgniter\Shield\Entities\User;
+use Faker\Generator;
+
+class ItemCalculationModel extends BaseResourceModel
+{
+    protected $table = "item_calculations";
+    protected $returnType = ItemCalculation::class;
+    protected $allowedFields = [
+        "frozen_account_hash",
+        "cash_flow_activity_id",
+        "remaining_quantity"
+    ];
+    protected $useTimestamps = false;
+    protected $useSoftDeletes = false;
+
+    protected $sortable_fields = [];
+
+    public function fake(Generator &$faker)
+    {
+        return [];
+    }
+
+    public function limitSearchToUser(BaseResourceModel $query_builder, User $user)
+    {
+        return $query_builder
+            ->whereIn(
+                "frozen_account_hash",
+                model(FrozenAccountModel::class, false)
+                    ->builder()
+                    ->select("hash")
+                    ->whereIn(
+                        "frozen_period_id",
+                        model(FrozenPeriodModel::class, false)
+                            ->builder()
+                            ->select("id")
+                            ->where("user_id", $user->id)
+                    )
+                    ->whereIn(
+                        "account_id",
+                        model(AccountModel::class, false)
+                            ->builder()
+                            ->select("id")
+                            ->whereIn(
+                                "currency_id",
+                                model(CurrencyModel::class, false)
+                                    ->builder()
+                                    ->select("id")
+                                    ->whereIn(
+                                        "precision_format_id",
+                                        model(PrecisionFormatModel::class, false)
+                                            ->builder()
+                                            ->select("id")
+                                            ->where("user_id", $user->id)
+                                    )
+                            )
+                    )
+            )->whereIn(
+                "financial_entry_id",
+                model(CashFlowActivityModel::class, false)
+                    ->builder()
+                    ->select("id")
+                    ->where("user_id", $user->id)
+            );
+    }
+
+    public static function extractLinkedCashFlowActivities(array $flow_calculations): array
+    {
+        return array_map(
+            fn ($flow_calculation) => $flow_calculation->cash_flow_activity_id,
+            $flow_calculations
+        );
+    }
+}
