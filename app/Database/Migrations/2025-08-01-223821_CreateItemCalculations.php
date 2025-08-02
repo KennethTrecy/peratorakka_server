d89eb8b46f979c42e9c87ad0be5b0b6def52b42d
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-08-02 12:15:23 +0800

    internal(frozen period): make migration for item calculations

---
+++ app/Database/Migrations/2025-08-01-223821_CreateItemCalculations.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+use Config\Database;
+
+class CreateItemCalculations extends Migration
+{
+    public function up()
+    {
+        $database = Database::connect();
+
+        $this->forge->addField([
+            "frozen_account_hash" => [
+                "type" => "CHARACTER",
+                "constraint" => 72
+            ],
+            "financial_entry_id" => [
+                "type" => "BIGINT",
+                "unsigned" => true
+            ],
+            "remaining_quantity" => [
+                "type" => "TEXT"
+            ]
+        ]);
+        if ($database->DBDriver !== "SQLite3") {
+            $this->forge->addForeignKey(
+                "frozen_account_hash",
+                "frozen_accounts",
+                "hash",
+                "CASCADE",
+                "CASCADE",
+                "item_calculations_frozen_account_hash_foreign"
+            );
+            $this->forge->addForeignKey(
+                "financial_entry_id",
+                "financial_entries_v2",
+                "id",
+                "CASCADE",
+                "CASCADE",
+                "item_calculations_financial_entry_id_foreign"
+            );
+        }
+        $this->forge->createTable("item_calculations");
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable("item_calculations");
+    }
+}
