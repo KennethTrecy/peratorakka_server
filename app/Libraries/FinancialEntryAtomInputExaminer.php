0c0f0f1c3a7a8a77203b5b7c59a2ee2b3422ba7f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2025-08-08 21:59:18 +0800

    fix(library): correct balance checking for known financial entries

--- app/Libraries/FinancialEntryAtomInputExaminer.php
+++ app/Libraries/FinancialEntryAtomInputExaminer.php
@@ -51,9 +51,9 @@ class FinancialEntryAtomInputExaminer extends InputExaminer
         $modifier_cache = ModifierCache::make($this->context);
         $modifier_cache->loadResources([ $modifier_id ]);
         $modifier_action = $modifier_cache->determineModifierAction($modifier_id);
+        $this->loadAssociatedAccounts();
 
-        $modifier_atom_cache = ModifierAtomCache::make($this->context);
-
+        // TODO: Prevent negative quantities for non-weight based itemized assets
         switch ($modifier_action) {
             case RECORD_MODIFIER_ACTION: {
                 $premade_financial_entry_atoms = $this->makeFinancialEntryAtoms($this->input);
@@ -69,12 +69,13 @@ class FinancialEntryAtomInputExaminer extends InputExaminer
 
             case CLOSE_MODIFIER_ACTION: {
                 $is_correct = count($this->input) === 2;
+                $is_balanced = $is_correct && $this->checkBalance($premade_financial_entry_atoms);
 
-                if ($is_correct) {
+                if ($is_balanced) {
                     $memoizer->write("#$modifier_id", []);
                 }
 
-                return $is_correct;
+                return $is_balanced;
             }
 
             case EXCHANGE_MODIFIER_ACTION: {
@@ -106,21 +107,18 @@ class FinancialEntryAtomInputExaminer extends InputExaminer
 
             case ASK_MODIFIER_ACTION: {
                 $premade_financial_entry_atoms = $this->makeFinancialEntryAtoms($this->input);
-                $filled_financial_entry_atoms = $this->fillSourceAndSinkAtomsAutomatically(
-                    $premade_financial_entry_atoms,
-                    REAL_DEBIT_MODIFIER_ATOM_KIND,
-                    REAL_CREDIT_MODIFIER_ATOM_KIND
-                );
 
-                // TODO: Transfer capital gains/losses from source atom to direct sale
-
-                $is_balanced = $this->checkBalance($filled_financial_entry_atoms);
-
-                if ($is_balanced) {
-                    $memoizer->write("#$modifier_id", $filled_financial_entry_atoms);
+                if (!$this->hasNecessaryItemizedInputs($premade_financial_entry_atoms)) {
+                    return false;
                 }
 
-                return $is_balanced;
+                $is_balanced = $this->checkBalance($premade_financial_entry_atoms);
+
+                if ($is_balanced) {
+                    $memoizer->write("#$modifier_id", $premade_financial_entry_atoms);
+                }
+
+                return true;
             }
 
                 // Make sure there is always an input to the accounts.
@@ -203,115 +201,12 @@ class FinancialEntryAtomInputExaminer extends InputExaminer
         return count($unresolved_itemized_entry_atoms) === 0;
     }
 
-    private function fillSourceAndSinkAtomsAutomatically(
-        array $premade_financial_entry_atoms,
-        string $source_side,
-        string $sink_side
-    ): array {
-        $keyed_financial_entry_atoms = Resource::key(
-            $premade_financial_entry_atoms,
-            fn ($atom) => $atom->modifier_atom_id
-        );
-
-        $expected_modifier_atoms = model(ModifierAtomModel::class, false)
-            ->where("modifier_id", $modifier_id)
-            ->findAll();
-
-        $modifier_atom_cache = ModifierAtomCache::make($this->context);
-        $modifier_atom_cache->addPreloadedResources($expected_modifier_atoms);
-
-        $account_cache = $this->context->getVariable(ContextKeys::ACCOUNT_CACHE);
-        $account_cache->loadResources(array_unique(array_map(
-            fn ($atom) => $atom->account_id,
-            $expected_modifier_atoms
-        )));
-
-        $used_modifier_atom_IDs = array_keys($keyed_financial_entry_atoms);
-        $unused_modifier_atom_IDs = array_diff(
-            array_map(fn ($atom) => $atom->id, $expected_modifier_atoms),
-            $used_modifier_atom_IDs
-        );
-        $grouped_modifier_atoms = array_map(
-            fn ($group) => Resource::group(
-                $group,
-                fn ($atom) => $account_cache->determineAccountKind($atom->account_id)
-            ),
-            Resource::group(
-                $expected_modifier_atoms,
-                fn ($atom) => $atom->kind
-            )
-        );
-
-        $price_modifier_atoms = Resource::key(
-            $grouped_modifier_atoms[PRICE_MODIFIER_ATOM_KIND][ITEMIZED_ASSET_ACCOUNT_KIND],
-            fn ($atom) => $atom->account_id
-        );
-        $item_count_modifier_atoms = Resource::key(
-            $grouped_modifier_atoms[ITEM_COUNT_MODIFIER_ATOM_KIND][ITEMIZED_ASSET_ACCOUNT_KIND],
-            fn ($atom) => $atom->account_id
-        );
-
-        $real_source_modifier_atoms = $grouped_modifier_atoms[$source_side];
-        $real_sink_modifier_atoms = $grouped_modifier_atoms[$sink_side];
-
-        if (count($real_source_modifier_atoms) !== 1 || count($real_sink_modifier_atoms) === 0) {
-            return [];
-        }
-
-        $source_modifier_atom = ($real_source_modifier_atoms[GENERAL_ASSET_ACCOUNT_KIND]
-            ?? $real_source_modifier_atoms[LIQUID_ASSET_ACCOUNT_KIND]
-            ?? $real_source_modifier_atoms[DEPRECIATIVE_ASSET_ACCOUNT_KIND])[0];
-        $sink_modifier_atoms = $real_sink_modifier_atoms[ITEMIZED_ASSET_ACCOUNT_KIND];
-
-        $sink_financial_entry_atoms = array_map(
-            function ($sink_modifier_atom) use (
-                $price_modifier_atoms,
-                $item_count_modifier_atoms,
-                $keyed_financial_entry_atoms
-            ) {
-                $financial_entry_atom_entity = new FinancialEntryAtom();
-                $price_value = $keyed_financial_entry_atoms[
-                    $price_modifier_atoms[$sink_modifier_atom->account_id]->id
-                ];
-                $quantity_value = $keyed_financial_entry_atoms[
-                    $item_count_modifier_atoms[$sink_modifier_atom->account_id]->id
-                ];
-                $financial_entry_atom_entity->fill([
-                    "modifier_atom_id" => $sink_modifier_atom->id,
-                    "numerical_value" => $price_value->multipliedBy($quantity_value),
-                ]);
-                return $financial_entry_atom_entity;
-            },
-            $sink_modifier_atoms
-        );
-
-        $source_atom = new FinancialEntryAtom();
-        $source_atom->modifier_atom_id = $source_modifier_atom->id;
-        return [
-            ...$premade_financial_entry_atoms,
-            array_reduce(
-                $sink_financial_entry_atoms,
-                fn ($previous_atom, $sink_atom) => $previous_atom->fill([
-                    "numerical_value" => $previous_atom->numerical_value->plus(
-                        $sink_atom->numerical_value
-                    )
-                ]),
-                $source_atom
-            ),
-            ...$sink_financial_entry_atoms
-        ];
-    }
-
     private function checkBalance(array $financial_entry_atoms): bool
     {
+        $modifier_cache = ModifierCache::make($this->context);
         $modifier_atom_cache = ModifierAtomCache::make($this->context);
         $account_cache = AccountCache::make($this->context);
-        $account_cache->loadResources(array_unique(array_map(
-            fn ($financial_entry_atom) => $modifier_atom_cache->determineModifierAtomAccountID(
-                $financial_entry_atom->modifier_atom_id
-            ),
-            $financial_entry_atoms
-        )));
+        $this->loadAssociatedAccounts();
 
         $unresolved_itemized_entry_atoms = [];
 
@@ -400,5 +295,17 @@ class FinancialEntryAtomInputExaminer extends InputExaminer
         $is_balanced = $real_debit_total->minus($real_credit_total)->isZero();
 
         return $is_balanced;
+    }
+
+    private function loadAssociatedAccounts() {
+        $modifier_atom_cache = ModifierAtomCache::make($this->context);
+        $modifier_atom_IDs = $this->extractModifierAtomIDs();
+        $modifier_atom_cache->loadResources($modifier_atom_IDs);
+        $modifier_atoms = Resource::key(
+            $modifier_atom_cache->getLoadedResources($modifier_atom_IDs),
+            fn ($resource) => $resource->id
+        );
+        $account_cache = AccountCache::make($this->context);
+        $account_cache->loadResources($modifier_atom_cache->extractAssociatedAccountIDs());
     }
 }
