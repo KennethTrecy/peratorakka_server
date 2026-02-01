c19449c4e4555f1fa0a7f5490194d3d618914524
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2026-02-01 17:58:50 +0800

    feat(flow calculation)!: drop deprecated table

---
+++ app/Database/Migrations/2026-02-01-095705_DropFlowCalculationsTable.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use Config\Database;
+
+class DropFlowCalculationsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->dropTable("flow_calculations");
+    }
+
+    public function down()
+    {
+        $database = Database::connect();
+
+        $this->forge->addField([
+            "id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+                "auto_increment" => true,
+            ],
+            "frozen_period_id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+            ],
+            "cash_flow_activity_id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+            ],
+            "account_id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+            ],
+            "net_amount" => [
+                "type" => "TEXT",
+            ]
+        ]);
+        $this->forge->addPrimaryKey("id", "pk_flow_calculations");
+        if ($database->DBDriver !== "SQLite3") {
+            $this->forge->addForeignKey(
+                "frozen_period_id",
+                "frozen_periods",
+                "id",
+                "CASCADE",
+                "CASCADE",
+                "flow_calculations_frozen_period_id_foreign"
+            );
+            $this->forge->addForeignKey(
+                "cash_flow_activity_id",
+                "cash_flow_activities",
+                "id",
+                "CASCADE",
+                "CASCADE",
+                "flow_calculations_cash_flow_activity_id_foreign"
+            );
+            $this->forge->addForeignKey(
+                "account_id",
+                "accounts",
+                "id",
+                "CASCADE",
+                "CASCADE",
+                "flow_calculations_account_id_foreign"
+            );
+        }
+        $this->forge->createTable("flow_calculations");
+    }
+}
