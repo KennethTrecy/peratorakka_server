316ef88d8fd960672e573d60519514c42ae18de5
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2026-02-01 17:36:11 +0800

    feat(financial entry)!: drop deprecated table

---
+++ app/Database/Migrations/2026-02-01-092750_DropFinancialEntries.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+use Config\Database;
+
+class DropFinancialEntries extends Migration
+{
+    public function up()
+    {
+        $this->forge->dropTable("financial_entries");
+    }
+
+    public function down()
+    {
+        $database = Database::connect();
+
+        $this->forge->addField([
+            "id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+                "auto_increment" => true,
+            ],
+            "modifier_id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+            ],
+            "transacted_at" => [
+                "type" => "DATETIME",
+            ],
+            "debit_amount" => [
+                "type" => "VARCHAR",
+                "constraint" => "255",
+            ],
+            "credit_amount" => [
+                "type" => "VARCHAR",
+                "constraint" => "255",
+            ],
+            "remarks" => [
+                "type" => "TEXT",
+                "null" => true,
+            ],
+            "created_at" => [
+                "type" => "DATETIME",
+                "default" => new RawSql("CURRENT_TIMESTAMP"),
+            ],
+            "updated_at" => [
+                "type" => "DATETIME",
+                "default" => new RawSql("CURRENT_TIMESTAMP"),
+            ],
+            "deleted_at" => [
+                "type" => "DATETIME",
+                "null" => true,
+            ]
+        ]);
+        $this->forge->addPrimaryKey("id", "pk_financial_entries");
+        if ($database->DBDriver !== "SQLite3") {
+            $this->forge->addForeignKey(
+                "modifier_id",
+                "modifiers",
+                "id",
+                "CASCADE",
+                "CASCADE",
+                "financial_entries_modifier_id_foreign"
+            );
+        }
+        $this->forge->createTable("financial_entries");
+    }
+}
