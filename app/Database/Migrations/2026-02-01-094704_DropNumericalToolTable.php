42689934d53b15d8e054df882b615995221a163e
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2026-02-01 17:48:20 +0800

    feat(numerical tool)!: drop deprecated table

---
+++ app/Database/Migrations/2026-02-01-094704_DropNumericalToolTable.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+use Config\Database;
+
+class DropNumericalToolTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->dropTable("numerical_tools");
+    }
+
+    public function down()
+    {
+        $database = Database::connect();
+
+        $this->forge->addField([
+            "id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+                "auto_increment" => true,
+            ],
+            "user_id" => [
+                "type" => "INT",
+                "unsigned" => true,
+            ],
+            "name" => [
+                "type" => "VARCHAR",
+                "constraint" => "255",
+            ],
+            "kind" => [
+                "type" => "INT",
+                "unsigned" => true,
+            ],
+            "recurrence" => [
+                "type" => "INT",
+                "unsigned" => true
+            ],
+            "recency" => [
+                "type" => "INT",
+                "unsigned" => false
+            ],
+            "order" => [
+                "type" => "INT",
+                "unsigned" => true
+            ],
+            "notes" => [
+                "type" => "TEXT",
+                "null" => true,
+            ],
+            "configuration" => [
+                "type" => "TEXT"
+            ],
+            "created_at" => [
+                "type" => "DATETIME",
+                "default" => new RawSql("CURRENT_TIMESTAMP"),
+            ],
+            "updated_at" => [
+                "type" => "DATETIME",
+                "default" => new RawSql("CURRENT_TIMESTAMP"),
+            ],
+            "deleted_at" => [
+                "type" => "DATETIME",
+                "null" => true,
+            ]
+        ]);
+        $this->forge->addPrimaryKey("id", "pk_numerical_tools");
+        if ($database->DBDriver !== "SQLite3") {
+            $this->forge->addForeignKey(
+                "user_id",
+                "users",
+                "id",
+                "CASCADE",
+                "CASCADE",
+                "collections_user_id_foreign"
+            );
+        }
+        $this->forge->createTable("numerical_tools");
+    }
+}
