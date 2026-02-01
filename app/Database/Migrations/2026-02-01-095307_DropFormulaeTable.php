64aa2057857f496198d9ce98411e62bd9acefa65
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2026-02-01 17:55:40 +0800

    feat(formulae)!: drop deprecated table

---
+++ app/Database/Migrations/2026-02-01-095307_DropFormulaeTable.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class DropFormulaeTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->dropTable("formulae");
+    }
+
+    public function down()
+    {
+        $database = Database::connect();
+
+        $this->forge->addField([
+            "id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+                "auto_increment" => true,
+            ],
+            "currency_id" => [
+                "type" => "BIGINT",
+                "unsigned" => true,
+            ],
+            "name" => [
+                "type" => "VARCHAR",
+                "constraint" => "255",
+            ],
+            "description" => [
+                "type" => "TEXT",
+                "null" => true,
+            ],
+            "output_format" => [
+                "type" => "INT",
+                "unsigned" => true,
+            ],
+            "exchange_rate_basis" => [
+                "type" => "INT",
+                "unsigned" => true,
+            ],
+            "presentational_precision" => [
+                "type" => "INT",
+                "unsigned" => true
+            ],
+            "formula" => [
+                "type" => "TEXT"
+            ],
+            "created_at" => [
+                "type" => "DATETIME",
+                "default" => new RawSql("CURRENT_TIMESTAMP"),
+            ],
+            "updated_at" => [
+                "type" => "DATETIME",
+                "default" => new RawSql("CURRENT_TIMESTAMP"),
+            ],
+            "deleted_at" => [
+                "type" => "DATETIME",
+                "null" => true,
+            ]
+        ]);
+        $this->forge->addPrimaryKey("id", "pk_formulae");
+        if ($database->DBDriver !== "SQLite3") {
+            $this->forge->addUniqueKey([ "user_id", "name" ], "formulae_user_id_name");
+            $this->forge->addForeignKey(
+                "currency_id",
+                "currencies",
+                "id",
+                "CASCADE",
+                "CASCADE",
+                "formulae_currency_id_foreign"
+            );
+        }
+        $this->forge->createTable("formulae");
+    }
+}
